6d027b6b759e07edd68ac6de5d193233c2faafd7
yingh <yinghu.lu@gmail.com>
2025-05-08 00:10:23 -0700

    gc migartion to go

--- internal/cluster/etcd.go
+++ internal/cluster/etcd.go
@@ -3,6 +3,7 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	clientv3 "go.etcd.io/etcd/client/v3"
@@ -23,11 +24,14 @@ func (c *Cluster) Watch() error {
 		return err
 	}
 	defer cli.Close()
-	rch := cli.Watch(context.Background(), c.Group, clientv3.WithPrefix())
-	for wresp := range rch {
+	wch := cli.Watch(context.Background(), c.Group, clientv3.WithPrefix())
+	for wresp := range wch {//blocked
 		for _, ev := range wresp.Events {
 			fmt.Printf("%s %q : %q\n", ev.Type, ev.Kv.Key, ev.Kv.Value)
-			cli.Close()
+			cmds := strings.Split(string(ev.Kv.Key), "#")
+			if cmds[1] == "quit" {
+				cli.Close()
+			}
 		}
 	}
 	return nil
