81cd82f9b462e3a63d0e23300ed90dd22d7b2dbf
yingh <yinghu.lu@gmail.com>
2025-05-21 10:44:56 -0700

    repackage gc code

--- internal/event/socket_data_buffer.go
+++ internal/event/socket_data_buffer.go
@@ -24,7 +24,7 @@ func (s *SocketBuffer) ReadInt8() (int8, error) {
 	if n != 1 {
 		return 0, errors.New("less than 1 bytes")
 	}
-	buf := bytes.NewBuffer(s.Buffer[:4])
+	buf := bytes.NewBuffer(s.Buffer[:1])
 	var v int8
 	if binary.Read(buf, binary.BigEndian, &v) != nil {
 		return 0, errors.New("wrong data convert")
@@ -35,6 +35,42 @@ func (s *SocketBuffer) ReadInt8() (int8, error) {
 func (s *SocketBuffer) WriteInt8(data int8) error {
 	buf := new(bytes.Buffer)
 	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	n, err := s.Socket.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != 1 {
+		return errors.New("less than 1 bytes")
+	}
+	return nil
+}
+
+func (s *SocketBuffer) ReadBool() (bool, error) {
+	n, err := s.Socket.Read(s.Buffer[:1])
+	if err != nil {
+		return false, err
+	}
+	if n != 1 {
+		return false, errors.New("less than 1 bytes")
+	}
+	buf := bytes.NewBuffer(s.Buffer[:1])
+	var v int8
+	if binary.Read(buf, binary.BigEndian, &v) != nil {
+		return false, errors.New("wrong data convert")
+	}
+	return v==1, nil
+}
+
+func (s *SocketBuffer) WriteBool(data bool) error {
+	buf := new(bytes.Buffer)
+	var bv int8 = 0
+	if data {
+		bv = 1
+	}
+	err := binary.Write(buf, binary.BigEndian, bv)
 	if err != nil {
 		return err
 	}
@@ -140,6 +176,134 @@ func (s *SocketBuffer) WriteInt64(data int64) error {
 	}
 	if n != 8 {
 		return errors.New("less than 8 bytes")
+	}
+	return nil
+}
+
+func (s *SocketBuffer) ReadFloat32() (float32, error) {
+	n, err := s.Socket.Read(s.Buffer[:4])
+	if err != nil {
+		return 0, err
+	}
+	if n != 4 {
+		return 0, errors.New("less than 4 bytes")
+	}
+	buf := bytes.NewBuffer(s.Buffer[:4])
+	var v float32
+	if binary.Read(buf, binary.BigEndian, &v) != nil {
+		return 0, errors.New("wrong data convert")
+	}
+	return v, nil
+}
+
+func (s *SocketBuffer) WriteFloat32(data float32) error {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	n, err := s.Socket.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != 4 {
+		return errors.New("less than 4 bytes")
+	}
+	return nil
+}
+
+func (s *SocketBuffer) ReadFloat64() (float64, error) {
+	n, err := s.Socket.Read(s.Buffer[:8])
+	if err != nil {
+		return 0, err
+	}
+	if n != 8 {
+		return 0, errors.New("less than 8 bytes")
+	}
+	buf := bytes.NewBuffer(s.Buffer[:8])
+	var v float64
+	if binary.Read(buf, binary.BigEndian, &v) != nil {
+		return 0, errors.New("wrong data convert")
+	}
+	return v, nil
+}
+
+func (s *SocketBuffer) WriteFloat64(data float64) error {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	n, err := s.Socket.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != 8 {
+		return errors.New("less than 8 bytes")
+	}
+	return nil
+}
+
+func (s *SocketBuffer) ReadComplex64() (complex64, error) {
+	n, err := s.Socket.Read(s.Buffer[:8])
+	if err != nil {
+		return 0, err
+	}
+	if n != 8 {
+		return 0, errors.New("less than 8 bytes")
+	}
+	buf := bytes.NewBuffer(s.Buffer[:8])
+	var v complex64
+	if binary.Read(buf, binary.BigEndian, &v) != nil {
+		return 0, errors.New("wrong data convert")
+	}
+	return v, nil
+}
+
+func (s *SocketBuffer) WriteComplex64(data complex64) error {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	n, err := s.Socket.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != 8 {
+		return errors.New("less than 8 bytes")
+	}
+	return nil
+}
+
+func (s *SocketBuffer) ReadComplex128() (complex128, error) {
+	n, err := s.Socket.Read(s.Buffer[:16])
+	if err != nil {
+		return 0, err
+	}
+	if n != 8 {
+		return 0, errors.New("less than 16 bytes")
+	}
+	buf := bytes.NewBuffer(s.Buffer[:16])
+	var v complex128
+	if binary.Read(buf, binary.BigEndian, &v) != nil {
+		return 0, errors.New("wrong data convert")
+	}
+	return v, nil
+}
+
+func (s *SocketBuffer) WriteComplex128(data complex128) error {
+	buf := new(bytes.Buffer)
+	err := binary.Write(buf, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	n, err := s.Socket.Write(buf.Bytes())
+	if err != nil {
+		return err
+	}
+	if n != 16 {
+		return errors.New("less than 16 bytes")
 	}
 	return nil
 }
