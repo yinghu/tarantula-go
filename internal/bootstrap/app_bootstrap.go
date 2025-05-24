4ad6eda437423f69317d817f120d705b78a747f2
yingh <yinghu.lu@gmail.com>
2025-05-24 12:55:11 -0700

    repackage gc code

---
+++ internal/bootstrap/app_bootstrap.go
@@ -0,0 +1,46 @@
+package bootstrap
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"gameclustering.com/internal/cluster"
+	"gameclustering.com/internal/conf"
+	"gameclustering.com/internal/event"
+)
+
+func AppBootstrap(service TarantulaService) {
+	f := conf.Env{}
+	f.Load(service.Config())
+	c := cluster.NewEtc(f.GroupName, f.PartitionNumber, f.EtcdEndpoints, cluster.Node{Name: f.NodeName, HttpEndpoint: f.HttpEndpoint, TcpEndpoint: f.TcpEndpoint})
+	e := event.Endpoint{TcpEndpoint: f.TcpEndpoint, Service: service, ReadBufferSize: f.TcpReadBufferSize}
+	go func() {
+		c.Started.Wait()
+		for v := range c.View() {
+			fmt.Printf("View :%v\n", v)
+		}
+		err := service.Start(f, &c)
+		if err!=nil{
+			fmt.Printf("Error %s\n",err.Error())
+		}
+	}()
+	go func() {
+		c.Started.Wait()
+		e.Open()
+	}()
+	go func() {
+		c.Started.Wait()
+		fmt.Println("Wating for signal to exit ...")
+		sigs := make(chan os.Signal, 1)
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		signal.Stop(sigs)
+		c.Quit <- true
+		service.Shutdown()
+		e.Close()
+		close(sigs)
+	}()
+	c.Join()
+}
