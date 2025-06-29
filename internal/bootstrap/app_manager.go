3d4a915021d5a02f2c0b4077d9463df0daeb97e4
yingh <yinghu.lu@gmail.com>
2025-06-29 10:25:07 -0700

    cluster listener for none admin app

--- internal/bootstrap/app_manager.go
+++ internal/bootstrap/app_manager.go
@@ -23,6 +23,7 @@ type AppManager struct {
 	Sql        persistence.Postgresql
 	ctx        string
 	standalone bool
+	appAuth    core.Authenticator
 }
 
 func (s *AppManager) ItemService() item.ItemService {
@@ -43,46 +44,16 @@ func (s *AppManager) Start(f conf.Env, c core.Cluster) error {
 	s.cls = c
 	s.ctx = f.GroupName
 	s.standalone = f.Standalone
-	tkn := util.JwtHMac{Alg: core.JWT_ALG, Ksz: core.JWT_KEY_SIZE}
-	ci := util.Aes{Ksz: core.CIPHER_KEY_SIZE}
-	err := c.Atomic(f.Presence, func(ctx core.Ctx) error {
-		jsk, err := ctx.Get(core.JWT_KEY_NAME)
-		if err != nil {
-			core.AppLog.Println("Create new jwt key")
-			nkey := util.Key(tkn.Ksz)
-			ctx.Put(core.JWT_KEY_NAME, util.KeyToBase64(nkey))
-			tkn.HMacFromKey(nkey)
-			return nil
-		}
-		jk, err := util.KeyFromBase64(jsk)
-		if err != nil {
-			return err
-		}
-		tkn.HMacFromKey(jk)
+	au, err := s.LoadAuth(f.Presence, f.GroupName)
+	if err != nil {
 		return nil
-	})
+	}
+	s.auth = au
+	ap, err := s.LoadAuth("presence", "presence")
 	if err != nil {
 		return err
 	}
-	err = c.Atomic(f.Presence, func(ctx core.Ctx) error {
-		csk, err := ctx.Get(core.CIPHER_KEY_NAME)
-		if err != nil {
-			core.AppLog.Println("Create new cipher key")
-			ckey := util.Key(ci.Ksz)
-			ctx.Put(core.CIPHER_KEY_NAME, util.KeyToBase64(ckey))
-			ci.AesGcmFromKey(ckey)
-		}
-		ck, err := util.KeyFromBase64(csk)
-		if err != nil {
-			return err
-		}
-		ci.AesGcmFromKey(ck)
-		return nil
-	})
-	if err != nil {
-		return err
-	}
-	s.auth = &AuthManager{Tkn: &tkn, Cipher: &ci, Kid: f.GroupName}
+	s.appAuth = ap
 	sql := persistence.Postgresql{Url: f.Pgs.DatabaseURL}
 	err = sql.Create()
 	if err != nil {
@@ -101,6 +72,7 @@ func (s *AppManager) Start(f conf.Env, c core.Cluster) error {
 		return err
 	}
 	s.imse = &is
+
 	c.Started()
 	return nil
 }
@@ -130,7 +102,7 @@ func (s *AppManager) MemberJoined(joined core.Node) {
 		return
 	}
 	core.AppLog.Printf("Member joined %v\n", joined)
-	tick, err := s.auth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
+	tick, err := s.appAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
 	if err != nil {
 		return
 	}
@@ -165,4 +137,47 @@ func (s *AppManager) Context() string {
 
 func (s *AppManager) Service() TarantulaService {
 	return s
+}
+
+func (s *AppManager) LoadAuth(context string, group string) (core.Authenticator, error) {
+	tkn := util.JwtHMac{Alg: core.JWT_ALG, Ksz: core.JWT_KEY_SIZE}
+	ci := util.Aes{Ksz: core.CIPHER_KEY_SIZE}
+	err := s.cls.Atomic(context, func(ctx core.Ctx) error {
+		jsk, err := ctx.Get(core.JWT_KEY_NAME)
+		if err != nil {
+			core.AppLog.Println("Create new jwt key")
+			nkey := util.Key(tkn.Ksz)
+			ctx.Put(core.JWT_KEY_NAME, util.KeyToBase64(nkey))
+			tkn.HMacFromKey(nkey)
+			return nil
+		}
+		jk, err := util.KeyFromBase64(jsk)
+		if err != nil {
+			return err
+		}
+		tkn.HMacFromKey(jk)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	err = s.cls.Atomic(context, func(ctx core.Ctx) error {
+		csk, err := ctx.Get(core.CIPHER_KEY_NAME)
+		if err != nil {
+			core.AppLog.Println("Create new cipher key")
+			ckey := util.Key(ci.Ksz)
+			ctx.Put(core.CIPHER_KEY_NAME, util.KeyToBase64(ckey))
+			ci.AesGcmFromKey(ckey)
+		}
+		ck, err := util.KeyFromBase64(csk)
+		if err != nil {
+			return err
+		}
+		ci.AesGcmFromKey(ck)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return &AuthManager{Tkn: &tkn, Cipher: &ci, Kid: group}, nil
 }
