20332b14941681a2132358884d5e7c284c1d6e87
yingh <yinghu.lu@gmail.com>
2025-06-29 14:55:06 -0700

    cluster listener for none admin app

--- internal/bootstrap/app_admin.go
+++ internal/bootstrap/app_admin.go
@@ -19,7 +19,7 @@ func (s *AppAdmin) AccessControl() int32 {
 func (s *AppAdmin) Request(rs core.OnSession, w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	cmd := r.PathValue("cmd")
-	core.AppLog.Printf("Call on cmd %s\n", cmd)
+	core.AppLog.Printf("Call on cmd %s %v\n", cmd,s.Cluster().Local())
 	if cmd == "join" {
 		var join core.Node
 		json.NewDecoder(r.Body).Decode(&join)
