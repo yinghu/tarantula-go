76e7648760cc529e0f65bf9fafb75f2cff704dd8
yingh <yinghu.lu@gmail.com>
2025-06-30 13:40:30 -0700

    docker run

--- internal/bootstrap/app_cluster_listener.go
+++ internal/bootstrap/app_cluster_listener.go
@@ -14,51 +14,43 @@ func (s *AppManager) MemberJoined(joined core.Node) {
 		return
 	}
 	core.AppLog.Printf("Member joined %v\n", joined)
-	tick, err := s.AppAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
-	if err != nil {
-		return
-	}
-	data, err := json.Marshal(joined)
-	if err != nil {
-		return
-	}
-	go sendToApp(tick, "presence", "join", data)
-	go sendToApp(tick, "asset", "join", data)
-	go sendToApp(tick, "profile", "join", data)
-	go sendToApp(tick, "inventory", "join", data)
-	go sendToApp(tick, "tournament", "join", data)
+	go s.sendToApp("presence", "join", joined)
+	go s.sendToApp("asset", "join", joined)
+	go s.sendToApp("profile", "join", joined)
+	go s.sendToApp("inventory", "join", joined)
+	go s.sendToApp("tournament", "join", joined)
 }
 func (s *AppManager) MemberLeft(left core.Node) {
 	if s.standalone {
 		return
 	}
 	core.AppLog.Printf("Member left %v\n", left)
-	tick, err := s.AppAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
-	if err != nil {
-		return
-	}
-	data, err := json.Marshal(left)
-	if err != nil {
-		return
-	}
-	go sendToApp(tick, "presence", "left", data)
-	go sendToApp(tick, "asset", "left", data)
-	go sendToApp(tick, "profile", "left", data)
-	go sendToApp(tick, "inventory", "left", data)
-	go sendToApp(tick, "tournament", "left", data)
+	go s.sendToApp("presence", "left", left)
+	go s.sendToApp("asset", "left", left)
+	go s.sendToApp("profile", "left", left)
+	go s.sendToApp("inventory", "left", left)
+	go s.sendToApp("tournament", "left", left)
 }
 func (s *AppManager) Updated(key string, value string) {
 	core.AppLog.Printf("Key updated %s %s\n", key, value)
 }
 
-func sendToApp(ticket string, app string, cmd string, data []byte) {
+func (s *AppManager) sendToApp(app string, cmd string, node core.Node) {
+	tick, err := s.AppAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
+	if err != nil {
+		return
+	}
+	data, err := json.Marshal(node)
+	if err != nil {
+		return
+	}
 	client := &http.Client{}
 	req, err := http.NewRequest("POST", "http://"+app+":8080/"+app+"/admin/"+cmd, bytes.NewBuffer(data))
 	if err != nil {
 		return
 	}
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+ticket)
+	req.Header.Set("Authorization", "Bearer "+tick)
 	resp, err := client.Do(req)
 	if err != nil {
 		core.AppLog.Printf("Error %s\n", err.Error())
