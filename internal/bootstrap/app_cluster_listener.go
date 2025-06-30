735a1c6b8b5761244274021cbe18f6f41d0c7fa7
yingh <yinghu.lu@gmail.com>
2025-06-30 10:13:58 -0700

    docker run

---
+++ internal/bootstrap/app_cluster_listener.go
@@ -0,0 +1,78 @@
+package bootstrap
+
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"gameclustering.com/internal/core"
+)
+
+func (s *AppManager) MemberJoined(joined core.Node) {
+	if s.standalone {
+		return
+	}
+	core.AppLog.Printf("Member joined %v\n", joined)
+	tick, err := s.AppAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
+	if err != nil {
+		return
+	}
+	data, err := json.Marshal(joined)
+	if err != nil {
+		return
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://web/asset/admin/join", bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+tick)
+	resp, err := client.Do(req)
+	if err != nil {
+		core.AppLog.Printf("Error %s\n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	r, err := io.ReadAll(resp.Body)
+	if err != nil {
+		core.AppLog.Printf("Resp Error %s\n", err.Error())
+	}
+	core.AppLog.Printf("Response code : %d %s\n", resp.StatusCode, string(r))
+}
+func (s *AppManager) MemberLeft(left core.Node) {
+	if s.standalone {
+		return
+	}
+	core.AppLog.Printf("Member left %v\n", left)
+	tick, err := s.AppAuth.CreateTicket(1, 1, SUDO_ACCESS_CONTROL)
+	if err != nil {
+		return
+	}
+	data, err := json.Marshal(left)
+	if err != nil {
+		return
+	}
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", "http://web/asset/admin/left", bytes.NewBuffer(data))
+	if err != nil {
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer "+tick)
+	resp, err := client.Do(req)
+	if err != nil {
+		core.AppLog.Printf("Error %s\n", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	r, err := io.ReadAll(resp.Body)
+	if err != nil {
+		core.AppLog.Printf("Resp Error %s\n", err.Error())
+	}
+	core.AppLog.Printf("Response code : %d %s\n", resp.StatusCode, string(r))
+}
+func (s *AppManager) Updated(key string, value string) {
+	core.AppLog.Printf("Key updated %s %s\n", key, value)
+}
