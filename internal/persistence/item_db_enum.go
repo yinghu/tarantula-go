7147f65e5c8098a77841a7dd1e99bc1a682a117d
yingh <yinghu.lu@gmail.com>
2025-07-02 09:28:56 -0700

    docker run

---
+++ internal/persistence/item_db_enum.go
@@ -0,0 +1,81 @@
+package persistence
+
+import (
+	"context"
+	"errors"
+
+	"gameclustering.com/internal/item"
+	"github.com/jackc/pgx/v5"
+)
+
+func (db *ItemDB) SaveEnum(c item.Enum) error {
+	return db.Sql.Txn(func(tx pgx.Tx) error {
+		var id int32
+		err := tx.QueryRow(context.Background(), INSERT_ENUM, c.Name).Scan(&id)
+		if err != nil {
+			return err
+		}
+		c.Id = id
+		valid := false
+		for i := range c.Values {
+			p := c.Values[i]
+			_, err = tx.Exec(context.Background(), INSERT_ENUM_VALUE, id, p.Name, p.Value)
+			if err != nil {
+				valid = false
+				return err
+			}
+			valid = true
+		}
+		if !valid {
+			return errors.New("at least 1 enum value required")
+		}
+		return nil
+	})
+}
+
+func (db *ItemDB) ValidateEnum(c item.Enum) error {
+	if c.Name == "" {
+		return errors.New("name none empty string required")
+	}
+	if len(c.Values) == 0 {
+		return errors.New("at least 1 value required")
+	}
+	for i := range c.Values {
+		v := c.Values[i]
+		if v.Name == "" {
+			return errors.New("prop name none empty string required")
+		}
+	}
+	return nil
+}
+
+func (db *ItemDB) LoadEnum(cname string) (item.Enum, error) {
+	cat := item.Enum{Name: cname}
+	err := db.Sql.Query(func(row pgx.Rows) error {
+		err := row.Scan(&cat.Id)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, SELECT_ENUM_WITH_NAME, cname)
+	if err != nil {
+		return cat, err
+	}
+	if cat.Id == 0 {
+		return cat, errors.New("enum not existed")
+	}
+	cat.Values = make([]item.EnumValue, 0)
+	err = db.Sql.Query(func(row pgx.Rows) error {
+		var prop item.EnumValue
+		err := row.Scan(&prop.Name, &prop.Value)
+		if err != nil {
+			return err
+		}
+		cat.Values = append(cat.Values, prop)
+		return nil
+	}, SELECT_ENUM_VALUES_WITH_CID, cat.Id)
+	if err != nil {
+		return cat, errors.New("no enum value existed")
+	}
+	return cat, nil
+}
