d009f0284742e37a642caf03b714738c16def72d
yingh <yinghu.lu@gmail.com>
2025-07-08 00:16:11 -0700

    admin ui

--- cmd/web/admin/admin_web_index.go
+++ cmd/web/admin/admin_web_index.go
@@ -4,6 +4,7 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strings"
 
 	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/core"
@@ -19,13 +20,23 @@ func (s *AdminWebIndex) AccessControl() int32 {
 }
 func (s *AdminWebIndex) Request(rs core.OnSession, w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-	fn := r.PathValue("name");
+	fn := r.PathValue("name")
 	dest, err := os.OpenFile("site/"+fn, os.O_RDONLY, 0644)
 	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		session := core.OnSession{Successful: false, Message: err.Error()}
 		w.Write(util.ToJson(session))
 		return
 	}
+	if strings.HasSuffix(fn, ".js") {
+		w.Header().Set("Content-Type", "text/javascript")
+	} else if strings.HasSuffix(fn, ".css") {
+		w.Header().Set("Content-Type", "text/css")
+	} else {
+		w.Header().Set("Content-Type", "text/html")
+	}
+	w.WriteHeader(http.StatusOK)
 	defer dest.Close()
 	io.Copy(w, dest)
 }
