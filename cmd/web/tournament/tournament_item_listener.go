92fdfd56f69c85adbf0e67be7975e23cfee02cf3
yingh <yinghu.lu@gmail.com>
2025-07-30 13:38:08 -0700

    item api

--- cmd/web/tournament/tournament_item_listener.go
+++ cmd/web/tournament/tournament_item_listener.go
@@ -1,10 +1,22 @@
 package main
 
 import (
+	"encoding/json"
+
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/item"
 )
 
 func (a *TournamentService) OnUpdated(kv item.KVUpdate) {
 	core.AppLog.Printf("Item update call %v \n", kv)
+	var reg item.ConfigRegistration
+	err := json.Unmarshal([]byte(kv.Value), &reg)
+	if err != nil {
+		return
+	}
+	ins, err := a.ItemService().Loader().Load(reg.ItemId)
+	if err != nil {
+		return
+	}
+	core.AppLog.Printf("%v\n", ins)
 }
