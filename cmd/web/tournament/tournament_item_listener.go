6bc5f46368b8b9e85905f2d26f4a3d83dada5a14
yingh <yinghu.lu@gmail.com>
2025-07-30 15:06:59 -0700

    item api

--- cmd/web/tournament/tournament_item_listener.go
+++ cmd/web/tournament/tournament_item_listener.go
@@ -2,17 +2,26 @@ package main
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/item"
 )
 
 func (a *TournamentService) OnUpdated(kv item.KVUpdate) {
-	core.AppLog.Printf("Item update call %v \n", kv)
+	itemId, err := strconv.ParseInt(kv.Key, 10, 64)
+	if err != nil {
+		core.AppLog.Printf("Key should be int64 %s\n", kv.Key)
+	}
 	if kv.Opt.IsCreate || kv.IsModify {
 		var reg item.ConfigRegistration
 		err := json.Unmarshal([]byte(kv.Value), &reg)
 		if err != nil {
+			core.AppLog.Printf("Value should be json format %v\n", kv.Value)
+			return
+		}
+		if reg.ItemId != itemId {
+			core.AppLog.Printf("Key not matched %d : %d\n", itemId, reg.ItemId)
 			return
 		}
 		ins, err := a.ItemService().Loader().Load(reg.ItemId)
@@ -22,4 +31,6 @@ func (a *TournamentService) OnUpdated(kv item.KVUpdate) {
 		core.AppLog.Printf("%v\n", ins)
 		return
 	}
+	core.AppLog.Printf("Item released %d\n", itemId)
+
 }
