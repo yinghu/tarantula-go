4e8e0ae97795d07421b8fdbe12ea50ecef1e8c1b
yingh <yinghu.lu@gmail.com>
2025-08-01 11:13:20 -0700

    cache app

--- cmd/web/cache/cache_service.go
+++ cmd/web/cache/cache_service.go
@@ -4,19 +4,26 @@ import (
 	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/conf"
 	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/persistence"
 )
 
 type CacheService struct {
 	bootstrap.AppManager
+	Ds core.DataStore
 }
 
 func (s *CacheService) Config() string {
 	return "/etc/tarantula/cache-conf.json"
 }
 
-func (s *CacheService) Start(f conf.Env, c core.Cluster) error {
-	//s.ItemUpdater = s
-	s.AppManager.Start(f, c)
-	//s.createSchema()
+func (s *CacheService) Start(env conf.Env, c core.Cluster) error {
+	s.AppManager.Start(env, c)
+	path := env.LocalDir + "/store"
+	ds := persistence.Cache{InMemory: env.Bdg.InMemory, Path: path, Seq: s.Sequence()}
+	err := ds.Open()
+	if err != nil {
+		return err
+	}
+	s.Ds = &ds
 	return nil
 }
