cec1eb25a75f10c1525d134b395db77d79006aaf
yingh <yinghu.lu@gmail.com>
2025-08-02 22:32:12 -0700

    postoffice app

--- cmd/web/postoffice/postoffice_publish_endpoint.go
+++ cmd/web/postoffice/postoffice_publish_endpoint.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/event"
 	"gameclustering.com/internal/util"
 )
 
@@ -19,5 +21,20 @@ func (s *PostofficePublisher) AccessControl() int32 {
 func (s *PostofficePublisher) Request(rs core.OnSession, w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	key := r.PathValue("key")
+	var me event.MessageEvent
+	err := json.NewDecoder(r.Body).Decode(&me)
+	if err != nil {
+		w.Write(util.ToJson(core.OnSession{Successful: false, Message: err.Error()}))
+		return
+	}
 	w.Write(util.ToJson(core.OnSession{Successful: true, Message: key}))
+	view := s.Cluster().View()
+	for i := range view {
+		v := view[i]
+		if v.Name == s.Cluster().Local().Name {
+			continue
+		}
+		pub := event.SocketPublisher{Remote: v.TcpEndpoint}
+		pub.Publish(&me, "ticket")
+	}
 }
