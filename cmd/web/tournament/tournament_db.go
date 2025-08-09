4b5d107c5ccec8e7227bb41748b90dacf4d2ca34
yingh <yinghu.lu@gmail.com>
2025-08-09 00:46:50 -0700

    postoffice app

--- cmd/web/tournament/tournament_db.go
+++ cmd/web/tournament/tournament_db.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-	//"errors"
-	//"github.com/jackc/pgx/v5"
+const (
+	TOURNAMENT_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS tournament (id SERIAL PRIMARY KEY,tournament_id BIGINT NOT NULL,instance_id BIGINT NOT NULL,system_id BIGINT NOT NULL,score INTEGER NOT NULL,last_updated TIMESTAMP NOT NULL,UNIQUE(tournament_id,instance_id,system_id))"
 )
 
 func (s *TournamentService) createSchema() error {
-	
-
+	_, err := s.Sql.Exec(TOURNAMENT_SQL_SCHEMA)
+	if err != nil {
+		return err
+	}
 	return nil
 }
