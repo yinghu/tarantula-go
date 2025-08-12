4891564cf70b76bf813180911592f57bd6776e9e
yingh <yinghu.lu@gmail.com>
2025-08-11 23:35:03 -0700

    api tool

--- cmd/web/tournament/tournament_db.go
+++ cmd/web/tournament/tournament_db.go
@@ -1,11 +1,21 @@
 package main
 
 const (
-	TOURNAMENT_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS tournament (id SERIAL PRIMARY KEY,tournament_id BIGINT NOT NULL,instance_id BIGINT NOT NULL,system_id BIGINT NOT NULL,score INTEGER NOT NULL,last_updated TIMESTAMP NOT NULL,UNIQUE(tournament_id,instance_id,system_id))"
+	SCHEDULE_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS tournament_schedule (tournament_id BIGINT PRIMARY KEY,running BOOLEAN NOT NULL)"
+	INSTANCE_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS tournament_instance (instance_id BIGINT PRIMARY KEY,tournament_id BIGINT NOT NULL,start_time TIMESTAMP NOT NULL,close_time TIMESTAMP NOT NULL,end_time TIMESTAMP NOT NULL)"
+	ENTRY_SQL_SCHEMA    string = "CREATE TABLE IF NOT EXISTS tournament_entry (instance_id BIGINT NOT NULL,system_id BIGINT NOT NULL,score INTEGER NOT NULL,last_updated TIMESTAMP NOT NULL,PRIMARY KEY(instance_id,system_id))"
 )
 
 func (s *TournamentService) createSchema() error {
-	_, err := s.Sql.Exec(TOURNAMENT_SQL_SCHEMA)
+	_, err := s.Sql.Exec(SCHEDULE_SQL_SCHEMA)
+	if err != nil {
+		return err
+	}
+	_, err = s.Sql.Exec(INSTANCE_SQL_SCHEMA)
+	if err != nil {
+		return err
+	}
+	_, err = s.Sql.Exec(ENTRY_SQL_SCHEMA)
 	if err != nil {
 		return err
 	}
