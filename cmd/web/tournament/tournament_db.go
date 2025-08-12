5941154488b9fd90665e8b33a3d4e0e08764bb42
yingh <yinghu.lu@gmail.com>
2025-08-12 00:55:42 -0700

    api tool

--- cmd/web/tournament/tournament_db.go
+++ cmd/web/tournament/tournament_db.go
@@ -9,10 +9,10 @@ const (
 	INSTANCE_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS tournament_instance (instance_id BIGINT PRIMARY KEY,tournament_id BIGINT NOT NULL,start_time TIMESTAMP NOT NULL,close_time TIMESTAMP NOT NULL,end_time TIMESTAMP NOT NULL)"
 	ENTRY_SQL_SCHEMA    string = "CREATE TABLE IF NOT EXISTS tournament_entry (instance_id BIGINT NOT NULL,system_id BIGINT NOT NULL,score INTEGER NOT NULL,last_updated TIMESTAMP NOT NULL,PRIMARY KEY(instance_id,system_id))"
 
-	INSERT_SCHEDULE string = "INSERT INTO tournament_schedule AS ts (tournament_id,running) VALUES($1,$2) ON CONFLICT DO UPDATE SET ts.running = true WHERE ts.tournament_id = $3"
+	INSERT_SCHEDULE string = "INSERT INTO tournament_schedule AS ts (tournament_id,running) VALUES($1,$2) ON CONFLICT (tournament_id) DO UPDATE SET running = true WHERE ts.tournament_id = $3"
 	INSERT_INSTANCE string = "INSERT INTO tournament_instance (instance_id,tournament_id,start_time,close_time,end_time) VALUES($1,$2,$3,$4,$5)"
 
-	UPDATE_SCHEDULE string = "UPDATE tournament_schedule AS ts SET ts.running = false WHERE ts.tournament_id = $1"
+	UPDATE_SCHEDULE string = "UPDATE tournament_schedule AS ts SET running = false WHERE ts.tournament_id = $1"
 
 	SELECT_SCHEDULE string = "SELECT tournament_id FROM tournament_schedule WHERE running = $1"
 	SELECT_INSTANCE string = "SELECT instance_id,start_time,close_time,end_time WHERE tournament_id = $1"
