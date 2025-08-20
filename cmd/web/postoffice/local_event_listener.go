3ba8a82932abff2cf39979d22a8791c70ebf603f
yingh <yinghu.lu@gmail.com>
2025-08-20 13:21:08 -0700

    tournament app

---
+++ cmd/web/postoffice/local_event_listener.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/event"
+)
+
+type LocalEventListener struct {
+	*PostofficeService
+}
+
+func (s *LocalEventListener) OnError(e event.Event, err error) {
+	core.AppLog.Printf("On event error %v %s\n", e, err.Error())
+}
+
+func (s *LocalEventListener) OnEvent(e event.Event) {
+	core.AppLog.Printf("On event %v\n", e)
+}
