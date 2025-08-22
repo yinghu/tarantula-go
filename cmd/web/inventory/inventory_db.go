22781f67382999875a6ff29b382444d0bde7854e
yingh <yinghu.lu@gmail.com>
2025-08-22 00:02:46 -0700

    tournament app

--- cmd/web/inventory/inventory_db.go
+++ cmd/web/inventory/inventory_db.go
@@ -1,24 +1,19 @@
 package main
 
-import (
-	//"errors"
-	//"github.com/jackc/pgx/v5"
-)
+//"errors"
+//"github.com/jackc/pgx/v5"
 
 const (
-	ITEM_INDEX_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS item_index (id SERIAL PRIMARY KEY,name VARCHAR(100) NOT NULL UNIQUE,category VARCHAR(50) NOT NULL,file_index VARCHAR(100) NOT NULL,update_time TIMESTAMP DEFAULT NOW())"
-	INVENTORY_SQL_SCHEMA  string = "CREATE TABLE IF NOT EXISTS inventory (id SERIAL PRIMARY KEY,system_id BIGINT NOT NULL,item_id INTEGER NOT NULL, rechargeable BOOL NOT NULL,update_time TIMESTAMP NOT NULL, UNIQUE(system_id,item_id))"
-	//ASSET_INDEX_INSERT_UPDATE string = "INSERT INTO asset_index AS a (system_id,name,file_index,upload_time) VALUES($1,$2,$3,$4) ON CONFLICT (system_id,name) DO UPDATE SET file_index = $5, upload_time = $6  WHERE a.system_id = $7 AND a.name = $8"
-	//ASSET_INDEX_SELECT        string = "SELECT file_index FROM asset_index AS a WHERE a.system_id = $1 AND a.name = $2"
+	INVENTORY_SQL_SCHEMA      string = "CREATE TABLE IF NOT EXISTS inventory (id SERIAL PRIMARY KEY,system_id BIGINT NOT NULL,type_id VARCHAR(50) NOT NULL, rechargeable BOOL NOT NULL,amount INTEGER NOT NULL,update_time TIMESTAMP NOT NULL, UNIQUE(system_id,type_id))"
+	INVENTORY_ITEM_SQL_SCHEMA string = "CREATE TABLE IF NOT EXISTS inventory_item (id SERIAL PRIMARY KEY,inventory_id INTEGER NOT NULL,item_id BIGINT NOT NULL)"
 )
 
 func (s *InventoryService) createSchema() error {
-	
-	_, err := s.Sql.Exec(ITEM_INDEX_SQL_SCHEMA)
+	_, err := s.Sql.Exec(INVENTORY_SQL_SCHEMA)
 	if err != nil {
 		return err
 	}
-	_, err = s.Sql.Exec(INVENTORY_SQL_SCHEMA)
+	_, err = s.Sql.Exec(INVENTORY_ITEM_SQL_SCHEMA)
 	if err != nil {
 		return err
 	}
