888a54eb9855f2ccec4640555849df8bf60bd88b
yingh <yinghu.lu@gmail.com>
2025-11-17 12:37:11 -0800

    mj update

---
+++ app/web/mahjong/mahjong_discharge_event.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/event"
+	"gameclustering.com/internal/mj"
+)
+
+type MahjongDischargeEvent struct {
+	D []mj.Tile
+	event.EventObj
+}
+
+func (s *MahjongDischargeEvent) ClassId() int {
+	return M_DISCHARGE_CID
+}
+
+func (s *MahjongDischargeEvent) ETag() string {
+	return "discharge"
+}
+
+func (s *MahjongDischargeEvent) Write(buff core.DataBuffer) error {
+	
+	sz := len(s.D)
+	if err := buff.WriteInt32(int32(sz)); err != nil {
+		return err
+	}
+	for i := range s.D {
+		if err := s.D[i].Write(buff); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *MahjongDischargeEvent) Outbound(buff core.DataBuffer) error {
+	err := s.WriteKey(buff)
+	if err != nil {
+		s.Callback.OnError(s, err)
+		return err
+	}
+	err = s.Write(buff)
+	if err != nil {
+		s.Callback.OnError(s, err)
+		return err
+	}
+	return nil
+}
