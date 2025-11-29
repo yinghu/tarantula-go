8431f49e9799fc3b5e2e20397d3f9bf016f7d846
yingh <yinghu.lu@gmail.com>
2025-11-29 11:31:20 -0800

    mj update

--- app/web/mahjong/mahjong_play_token.go
+++ app/web/mahjong/mahjong_play_token.go
@@ -3,18 +3,18 @@ package main
 import "gameclustering.com/internal/core"
 
 const (
-	CMD_SIT           int = 0
-	CMD_DICE          int = 1
-	CMD_DEAL          int = 2
-	CMD_DRAW          int = 3
-	CMD_DISCHARGE     int = 4
-	CMD_PUNG          int = 5
-	CMD_KONG          int = 6
-	CMD_CHOW          int = 7
-	CMD_CLAIM         int = 8
-	CMD_TABLE         int = 9
-	CMD_SKIP          int = 10 //skip discharge event
-	
+	CMD_SIT       int = 0
+	CMD_DICE      int = 1
+	CMD_DEAL      int = 2
+	CMD_DRAW      int = 3
+	CMD_DISCHARGE int = 4
+	CMD_PUNG      int = 5
+	CMD_KONG      int = 6
+	CMD_CHOW      int = 7
+	CMD_CLAIM     int = 8
+	CMD_TABLE     int = 9
+	CMD_SKIP      int = 10 //skip discharge event
+
 	CMD_TURN_CONTINUE int = 96
 	CMD_TURN_END      int = 97
 	CMD_RESET         int = 98
@@ -56,6 +56,12 @@ func (mp *MahjongPlayToken) Write(buff core.DataBuffer) error {
 	if err := buff.WriteInt32(int32(mp.Selected)); err != nil {
 		return err
 	}
+	if err := buff.WriteInt32(int32(mp.Chow1)); err != nil {
+		return err
+	}
+	if err := buff.WriteInt32(int32(mp.Chow2)); err != nil {
+		return err
+	}
 	return nil
 }
 
@@ -90,5 +96,15 @@ func (mp *MahjongPlayToken) Read(buff core.DataBuffer) error {
 		return err
 	}
 	mp.Selected = int(selected)
+	chow1, err := buff.ReadInt32()
+	if err != nil {
+		return err
+	}
+	mp.Chow1 = int(chow1)
+	chow2, err := buff.ReadInt32()
+	if err != nil {
+		return err
+	}
+	mp.Chow2 = int(chow2)
 	return nil
 }
