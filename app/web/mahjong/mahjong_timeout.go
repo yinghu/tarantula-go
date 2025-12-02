49d455f0637c92acbabd608bc1606a73ee901268
yingh <yinghu.lu@gmail.com>
2025-12-02 13:09:07 -0800

    mj update

--- app/web/mahjong/mahjong_timeout.go
+++ app/web/mahjong/mahjong_timeout.go
@@ -32,10 +32,11 @@ type OnStop func(commited bool)
 
 type MahjongTimeoutObj struct {
 	MahjongEventObj
-	S chan StopSignal
+	//S chan StopSignal
 	N MahjongPlayTurn
 	T OnTurn //triger on timer
 	P OnStop //call on stop
+	K *time.Timer
 }
 type StopSignal struct {
 	Commited bool
@@ -43,24 +44,19 @@ type StopSignal struct {
 }
 
 func (s *MahjongTimeoutObj) Start(tb *MahjongTable) {
-	tm := *time.NewTimer(time.Duration(s.N.CountDown+COUNT_DOWN_BUFFER) * time.Second)
-	s.S = make(chan StopSignal)
-	defer func() {
-		tm.Stop()
-		close(s.S)
-		core.AppLog.Printf("timeout!!")
-	}()
-	for {
-		select {
-		case <-tm.C:
-			s.T()
-		case <-s.S:
-			return
-		}
-	}
+	s.K = time.NewTimer(time.Duration(s.N.CountDown+COUNT_DOWN_BUFFER) * time.Second)
+	//s.S = make(chan StopSignal)
+	//defer func() {
+	//close(s.S)
+	//core.AppLog.Printf("timeout!!")
+	//}()
+	<-s.K.C
+	s.T()
+	core.AppLog.Printf("timer called")
 }
 
 func (mt *MahjongTimeoutObj) Stop(commited bool, closing bool) {
-	mt.S <- StopSignal{Commited: commited, Closing: closing}
+	//mt.S <- StopSignal{Commited: commited, Closing: closing}
+	mt.K.Stop()
 	mt.P(commited)
 }
