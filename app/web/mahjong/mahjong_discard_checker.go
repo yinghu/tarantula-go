0499dd4cb6a9b04509fd08805421d58797c83826
yingh <yinghu.lu@gmail.com>
2025-12-05 11:29:08 -0800

    mj update

--- app/web/mahjong/mahjong_discard_checker.go
+++ app/web/mahjong/mahjong_discard_checker.go
@@ -1,8 +1,39 @@
 package main
 
-import "gameclustering.com/internal/core"
+import (
+	"slices"
+
+	"gameclustering.com/internal/core"
+)
+
+type HandSegmenet struct {
+	T int
+	C int
+}
+
+func hmp(a, b HandSegmenet) int {
+	return a.C - b.C
+}
 
 func CheckDiscard(p *MahjongPlayer) int {
-	core.AppLog.Printf("Distribution %v\n", p.TC)
+	tcs := make([]HandSegmenet, 0)
+	tcs = append(tcs,HandSegmenet{T:TC_H,C: p.TC[TC_H]})
+	tcs = append(tcs,HandSegmenet{T:TC_C,C: p.TC[TC_C]})
+	tcs = append(tcs,HandSegmenet{T:TC_B,C: p.TC[TC_B]})
+	tcs = append(tcs,HandSegmenet{T:TC_D,C: p.TC[TC_D]})
+	slices.SortFunc(tcs,hmp)
+	if p.TC[TC_H] > 0 {
+
+	}
+	if p.TC[TC_C] > 0 {
+
+	}
+	if p.TC[TC_B] > 0 {
+
+	}
+	if p.TC[TC_D] > 0 {
+
+	}
+	core.AppLog.Printf("Distribution %v\n",tcs)
 	return 0
 }
