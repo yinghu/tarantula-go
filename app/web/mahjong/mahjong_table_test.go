c68cd1dd7b103c8ba941ed7554742605b9461dcc
yingh <yinghu.lu@gmail.com>
2025-12-10 22:48:00 -0800

    mj update

---
+++ app/web/mahjong/mahjong_table_test.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"testing"
+
+	"gameclustering.com/internal/mj"
+)
+
+func TestTableSetup(t *testing.T) {
+	mt := MahjongTable{}
+	mt.New()
+	st, err := mt.Sit(100)
+	if err != nil {
+		t.Errorf("shoud not be error %s", err.Error())
+	}
+	if st != 0 {
+		t.Errorf("Seat should be 0 %d\n", st)
+	}
+	st, err = mt.Sit(200)
+	if err != nil {
+		t.Errorf("shoud not be error %s", err.Error())
+	}
+	if st != 1 {
+		t.Errorf("Seat should be 1 %d\n", st)
+	}
+	st, err = mt.Sit(300)
+	if err != nil {
+		t.Errorf("shoud not be error %s", err.Error())
+	}
+	if st != 2 {
+		t.Errorf("Seat should be 2 %d\n", st)
+	}
+	st, err = mt.Sit(400)
+	if err != nil {
+		t.Errorf("shoud not be error %s", err.Error())
+	}
+	if st != 3 {
+		t.Errorf("Seat should be 3 %d\n", st)
+	}
+	mt.Dice()
+	dealer, err := mt.Deal()
+	if err != nil {
+		t.Errorf("shoud not be error %s", err.Error())
+	}
+	d := (mt.Pts() - 1) % 4
+	if dealer != d {
+		t.Errorf("dealer should be %d %d\n", dealer, d)
+	}
+	mp := mt.Players[d]
+	hz := mp.Hand.TileSize()
+	if hz != 14 {
+		t.Errorf("dealer HAND should be 14 %d\n", hz)
+	}
+	if !mp.TN {
+		t.Errorf("dealer tn should be true %v\n", mp.TN)
+	}
+	var tc [4]int
+	//fz :=0
+	for x := range mp.Hand.Tiles {
+		tile := mp.Hand.Tiles[x]
+		if tile.Suit == mj.CHARACTER {
+			tc[TC_C]++
+		}
+		if tile.Suit == mj.BAMBOO {
+			tc[TC_B]++
+		}
+		if tile.Suit == mj.DOTS {
+			tc[TC_D]++
+		}
+		if tile.Suit == mj.HORNOR {
+			tc[TC_H]++
+		}
+		//if tile.Suit == mj.FLOWER{
+		//fz++
+		//}
+	}
+
+	if tc != mp.TC {
+		t.Errorf("should be same numbers %v == %v", tc, mp.TC)
+	}
+	if len(mp.C) != tc[TC_C] {
+		t.Errorf("Character number should not be %d : %d", len(mp.C), tc[TC_C])
+	}
+	if len(mp.D) != tc[TC_D] {
+		t.Errorf("dots number should not be %d : %d", len(mp.D), tc[TC_D])
+	}
+	if len(mp.B) != tc[TC_B] {
+		t.Errorf("bamboo number should not be %d : %d", len(mp.B), tc[TC_B])
+	}
+	hs := len(mp.HE) + len(mp.HS) + len(mp.HW) + len(mp.HN) + len(mp.R) + len(mp.G) + len(mp.W)
+	if hs != tc[TC_H] {
+		t.Errorf("hornor number should not be %d : %d", hs, tc[TC_H])
+
+	}
+	if mp.LD == 0 {
+		t.Errorf("last draw should not be 0 %d", mp.LD)
+	}
+
+	for i := range 4 {
+		if i != d {
+			p := mt.Players[i]
+			pz := p.Hand.TileSize()
+			if pz != 13 {
+				t.Errorf("player HAND should be 13 %d\n", pz)
+			}
+			if p.TN {
+				t.Errorf("player tn should be false %v\n", p.TN)
+			}
+			var pc [4]int
+			for x := range p.Hand.Tiles {
+				tile := p.Hand.Tiles[x]
+				if tile.Suit == mj.CHARACTER {
+					pc[TC_C]++
+				}
+				if tile.Suit == mj.BAMBOO {
+					pc[TC_B]++
+				}
+				if tile.Suit == mj.DOTS {
+					pc[TC_D]++
+				}
+				if tile.Suit == mj.HORNOR {
+					pc[TC_H]++
+				}
+			}
+			if pc != p.TC {
+				t.Errorf("should be same numbers %v == %v", pc, p.TC)
+			}
+			if len(p.C) != pc[TC_C] {
+				t.Errorf("Character number should not be %d : %d", len(p.C), pc[TC_C])
+			}
+			if len(p.D) != pc[TC_D] {
+				t.Errorf("dots number should not be %d : %d", len(p.D), pc[TC_D])
+			}
+			if len(p.B) != pc[TC_B] {
+				t.Errorf("bamboo number should not be %d : %d", len(p.B), pc[TC_B])
+			}
+			hs := len(p.HE) + len(p.HS) + len(p.HW) + len(p.HN) + len(p.R) + len(p.G) + len(p.W)
+			if hs != pc[TC_H] {
+				t.Errorf("hornor number should not be %d : %d", hs, pc[TC_H])
+
+			}
+			if p.LD == 0 {
+				t.Errorf("last draw should not be 0 %d", p.LD)
+			}
+		}
+	}
+
+}
