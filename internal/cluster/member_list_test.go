445a647d65535c5cfb9489c07db4b0177b7d69c4
yingh <yinghu.lu@gmail.com>
2026-01-22 00:23:35 -0800

    add member list test

--- internal/cluster/member_list_test.go
+++ internal/cluster/member_list_test.go
@@ -10,11 +10,11 @@ import (
 
 func TestMemberList(t *testing.T) {
 	cfg := memberlist.DefaultLANConfig()
-	ch := make(chan memberlist.NodeEvent,10)
+	ch := make(chan memberlist.NodeEvent, 10) //HAVE TO BUFFER
 	cl := memberlist.ChannelEventDelegate{Ch: ch}
-	cfg.Name = "a03"
+
 	cfg.Events = &cl
-	fmt.Printf("config %s %d %v\n", cfg.Name, cfg.BindPort, cfg.SecretKey)
+	fmt.Printf("config %s %d %v %s\n", cfg.Name, cfg.BindPort, cfg.SecretKey, cfg.Label)
 	list, err := memberlist.Create(cfg)
 	if err != nil {
 		fmt.Printf("Erorr %s\n", err.Error())
@@ -29,11 +29,11 @@ func TestMemberList(t *testing.T) {
 		}
 	}(list)
 	//fmt.Printf("joining to ")
-	//n, err := list.Join([]string{"localhost:7946"})
-	//if err != nil {
-		//panic("Failed to join cluster: " + err.Error())
-	//}
-	//fmt.Printf("joined : %d\n", n)
+	n, err := list.Join([]string{"192.168.1.11:7946", "192.168.1.6:7946"})
+	if err != nil {
+		panic("Failed to join cluster: " + err.Error())
+	}
+	fmt.Printf("joined : %d\n", n)
 	// Ask for members of the cluster
 	wg.Wait()
 }
