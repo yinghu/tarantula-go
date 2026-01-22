7594e136dfb6038531039c36c2735002f96a01e3
yingh <yinghu.lu@gmail.com>
2026-01-21 23:35:22 -0800

    add member list test

--- internal/cluster/member_list_test.go
+++ internal/cluster/member_list_test.go
@@ -12,7 +12,7 @@ func TestMemberList(t *testing.T) {
 	cfg := memberlist.DefaultLANConfig()
 	ch := make(chan memberlist.NodeEvent)
 	cl := memberlist.ChannelEventDelegate{Ch: ch}
-	cfg.Name = "a01"
+	cfg.Name = "a02"
 	cfg.Events = &cl
 
 	fmt.Printf("config %s %d %v\n", cfg.Name, cfg.BindPort, cfg.SecretKey)
@@ -24,13 +24,15 @@ func TestMemberList(t *testing.T) {
 	wg := sync.WaitGroup{}
 	wg.Add(1)
 	go func(m *memberlist.Memberlist) {
-		for e := range ch {
+		for {
+			e := <-ch
 			fmt.Printf("Node event %v\n", e)
 		}
 	}(list)
+	//fmt.Printf("joining to ")
 	//n, err := list.Join([]string{"192.168.1.11:7946"})
 	//if err != nil {
-	//panic("Failed to join cluster: " + err.Error())
+		//panic("Failed to join cluster: " + err.Error())
 	//}
 	//fmt.Printf("joined : %d\n", n)
 	// Ask for members of the cluster
