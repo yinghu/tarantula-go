ba9b1ccd297d16bf6ee48223b7053b9befdbb5dc
yingh <yinghu.lu@gmail.com>
2026-01-22 21:20:06 -0800

    add member list test

--- internal/bootstrap/go.mod
+++ internal/bootstrap/go.mod
@@ -64,11 +64,11 @@ require (
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 	go.yaml.in/yaml/v2 v2.4.2 // indirect
-	golang.org/x/crypto v0.41.0 // indirect
-	golang.org/x/net v0.43.0 // indirect
-	golang.org/x/sync v0.16.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
-	golang.org/x/text v0.28.0 // indirect
+	golang.org/x/crypto v0.44.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sync v0.18.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
 	google.golang.org/genproto/googleapis/api v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250303144028-a0af3efb3deb // indirect
 	google.golang.org/grpc v1.71.1 // indirect
--- internal/cluster/member_list_test.go
+++ internal/cluster/member_list_test.go
@@ -9,6 +9,30 @@ import (
 	"github.com/hashicorp/memberlist"
 )
 
+type MockDelegate struct {
+}
+
+func (M *MockDelegate) NodeMeta(limit int) []byte {
+	fmt.Printf("node meta %d\n", limit)
+	return nil
+}
+
+func (M *MockDelegate) NotifyMsg(msg []byte) {
+	fmt.Printf("notify msf %s\n", string(msg))
+}
+
+func (M *MockDelegate) GetBroadcasts(overhead, limit int) [][]byte {
+	fmt.Printf("broadcast %d %d\n", overhead, limit)
+	return nil
+}
+
+func (M *MockDelegate) LocalState(join bool) []byte {
+	fmt.Printf("node meta %v\n", join)
+	return nil
+}
+func (M *MockDelegate) MergeRemoteState(buf []byte, join bool) {
+	fmt.Printf("node meta %s %v\n", string(buf), join)
+}
 func TestMemberList(t *testing.T) {
 	core.CreateTestLog()
 	cfg := memberlist.DefaultLANConfig()
@@ -16,6 +40,8 @@ func TestMemberList(t *testing.T) {
 	cl := memberlist.ChannelEventDelegate{Ch: ch}
 	cfg.Logger = core.AppLog
 	cfg.Events = &cl
+	cfg.Delegate = &MockDelegate{}
+
 	fmt.Printf("config %s %d %v %s\n", cfg.Name, cfg.BindPort, cfg.SecretKey, cfg.Label)
 	list, err := memberlist.Create(cfg)
 	if err != nil {
@@ -30,12 +56,14 @@ func TestMemberList(t *testing.T) {
 			fmt.Printf("Node event %v %d\n", e, m.NumMembers())
 		}
 	}(list)
-	//fmt.Printf("joining to ")
 	n, err := list.Join([]string{"192.168.1.11:7946", "192.168.1.6:7946"})
 	if err != nil {
 		panic("Failed to join cluster: " + err.Error())
 	}
 	fmt.Printf("joined : %d\n", n)
+	list.SendReliable(list.LocalNode(), []byte("hello"))
+	list.SendBestEffort(list.LocalNode(),[]byte("udp"))
+	//list.Leave(5)
 	// Ask for members of the cluster
 	wg.Wait()
 }
