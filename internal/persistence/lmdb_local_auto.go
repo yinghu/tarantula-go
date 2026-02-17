d6b73f7662cf5d1b54cce224a37582cdcd1a0f28
yingh <yinghu.lu@gmail.com>
2026-02-17 11:50:03 -0800

    json log hooked with zerolog

--- internal/persistence/lmdb_local_auto.go
+++ internal/persistence/lmdb_local_auto.go
@@ -1,11 +1,24 @@
 package persistence
 
-type LMDBLocalAuto struct{
+import (
+	"gameclustering.com/internal/core"
+	"wellquite.org/golmdb"
+)
+
+type LMDBLocalAuto struct {
 	Path      string
 	MapSizeMb int64
-	Readers   int
+	Readers   uint
+	MaxDbs    uint
+	BatchSzie uint
+	Env       *golmdb.LMDBClient
 }
 
 func (db *LMDBLocalAuto) Open() {
-
+	var flag golmdb.EnvironmentFlag
+	env, err := golmdb.NewLMDB(core.AppLog, db.Path, 0666, db.Readers, db.MaxDbs, flag, db.BatchSzie)
+	if err != nil {
+		panic(err)
+	}
+	db.Env = env
 }
--- internal/persistence/lmdb_local_test.go
+++ internal/persistence/lmdb_local_test.go
@@ -3,6 +3,8 @@ package persistence
 import (
 	"fmt"
 	"testing"
+
+	"gameclustering.com/internal/core"
 	//"github.com/PowerDNS/lmdb-go/lmdb"
 )
 
@@ -18,4 +20,19 @@ func TestSimple(t *testing.T) {
 	}
 	v, _ := lm.Get("test", []byte("key"))
 	fmt.Printf("value : %s\n", string(v))
+}
+
+func TestAuto(t *testing.T) {
+	core.CreateTestLog()
+	lm := LMDBLocalAuto{Path: "/home/yinghu/auto", MapSizeMb: 100, Readers: 100, MaxDbs: 1024, BatchSzie: 16}
+	lm.Open()
+	defer lm.Env.TerminateSync()
+
+	//err := lm.Put("test", []byte("key2"), []byte("value"))
+	//if err != nil {
+	//fmt.Printf("put error %s\n", err.Error())
+	//return
+	//}
+	//v, _ := lm.Get("test", []byte("key"))
+	//fmt.Printf("value : %s\n", string(v))
 }
