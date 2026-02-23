f7188d3ffd3054ee3bd33a19245dd5eeb182b0ab
yingh <yinghu.lu@gmail.com>
2026-02-23 12:59:00 -0800

    repack grpc code

--- app/web/tarantula/presence/presence_set_value_endpoint.go
+++ app/web/tarantula/presence/presence_set_value_endpoint.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/util"
 )
 
 type PresenceSetValueEndpoint struct {
@@ -16,13 +17,13 @@ func (p *PresenceSetValueEndpoint) ServeHTTP(w http.ResponseWriter, r *http.Requ
 	w.Header().Set("Access-Control-Allow-Headers", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "*")
 	k := r.PathValue("key")
-	v := r.PathValue("value");
+	v := r.PathValue("value")
 	rq := make(chan core.Chunk, 3)
 	defer close(rq)
-	p.Set(core.SetRequest{Key: []byte(k),Value: []byte(v), Async: rq})
+	p.Set(core.SetRequest{Key: []byte(k), Value: []byte(v), Async: rq})
 	for c := range rq {
 		if len(c.Data) > 0 {
-			w.Write(c.Data)
+			w.Write(util.ToJson(c))
 		}
 		if !c.Remaining {
 			break
