aebb0238f60b4a36c1c0be336e9f144e0383ef33
yingh <yinghu.lu@gmail.com>
2026-02-26 14:45:45 -0800

    data balance

--- internal/core/cluster.go
+++ internal/core/cluster.go
@@ -10,7 +10,7 @@ type Node struct {
 	Meta         string `json:"meta"`
 	IP           string `json:"address"`
 	State        int    `json:"-"`
-	RpcEndpoint  string `json:"rpc,omitempty"`
+	RpcEndpoint  string `json:"rpc"`
 	HttpEndpoint string `json:"http,omitempty"`
 	TcpEndpoint  string `json:"tcp,omitempty"`
 }
