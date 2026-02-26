8de97cba933f6470b34c90f82e1846765c99b895
yingh <yinghu.lu@gmail.com>
2026-02-26 15:28:15 -0800

    data balance

--- app/web/tarantula/data_service_provider.go
+++ app/web/tarantula/data_service_provider.go
@@ -128,6 +128,7 @@ func (m *DataServiceProvider) RingUpdated() {
 					if m.Mll.localNode(ringRange[1]) {
 						//push local data from >= pre.hash to < added.hash to remote added node
 						core.AppLog.Debug().Msgf("push data key hash >= %d and < %d to remote node %s", ringRange[0].RingToken, n.RingToken, n.IP)
+						m.Mll.MRequest <- core.RingRequest{Opt: BALANCE_NODE_OPT, Address: n.IP}
 					}
 				}
 			case NODE_STATE_DEAD:
