09f87e8c4f77f13170401c327abd50f07a264690
yingh <yinghu.lu@gmail.com>
2026-02-28 23:51:04 -0800

    data balance"

--- app/web/tarantula/data_service_provider.go
+++ app/web/tarantula/data_service_provider.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"sync"
+	"time"
 
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/persistence"
@@ -74,6 +75,9 @@ func (c *DataServiceProvider) Set(ctx context.Context, in *protocol.Data) (*prot
 	msg := make(chan SetRes, 1)
 	defer close(msg)
 	setData := SetData{Data: in, Msg: msg}
+	setData.Header.Revision = 109
+	setData.Header.Timestamp = time.Now().UnixMilli()
+	setData.Header.Size = int32(len(setData.Value))
 	c.DSet <- setData
 	resp := <-msg
 	return &protocol.Response{Successful: resp.Suc}, resp.Err
