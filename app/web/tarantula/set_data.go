44f50c8d31b22b5cf71fcf2a943684021404c348
yingh <yinghu.lu@gmail.com>
2026-02-28 22:10:51 -0800

    grpc data opreation api

---
+++ app/web/tarantula/set_data.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+
+	"gameclustering.com/internal/core"
+	"gameclustering.com/tarantula/protocol"
+	hash "github.com/spaolacci/murmur3"
+)
+
+const (
+	COMPOSIT_KEY_MAX int = 500
+)
+
+type SetData struct {
+	*protocol.Data
+	Opt  int
+	Msg  chan SetRes
+}
+
+func (s *SetData) KeyIndex() KeyIndex {
+	ki := KeyIndex{Prefix: hash.Sum32(s.Key), Header: s.Header, Key: s.Key}
+	return ki
+}
+
+//HELPER METHODS
+func (s *SetData) K() ([]byte, error) {
+	ksz := len(s.Key)
+	if ksz+20 > COMPOSIT_KEY_MAX {
+		return []byte{}, fmt.Errorf("Key size overflow %d", ksz)
+	}
+	buffer := core.NewBuffer(COMPOSIT_KEY_MAX)
+	buffer.WriteInt32(s.Header.FactoryId)
+	buffer.WriteInt32(s.Header.ClassId)
+	buffer.WriteInt32(int32(ksz))
+	buffer.Write(s.Key)
+	buffer.WriteInt64(s.Header.Revision)
+	buffer.Flip()
+	return buffer.Read(0)
+}
+
