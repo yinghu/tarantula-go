151d8797ea4d8b424f5e1db1c89e8757cdcca3ed
yingh <yinghu.lu@gmail.com>
2026-03-03 09:06:07 -0800

    memberlist integration to replace etcd

--- internal/bootstrap/app_bootstrap.go
+++ internal/bootstrap/app_bootstrap.go
@@ -106,7 +106,6 @@ func Logging(s TarantulaApp) http.HandlerFunc {
 		w.Header().Set("Access-Control-Allow-Headers", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "*")
 		if s.AccessControl() == PUBLIC_ACCESS_CONTROL {
-
 			s.Request(core.OnSession{}, w, r)
 			return
 		}
