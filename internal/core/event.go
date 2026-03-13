f6d6626ee451f8052a1b0a2e31e1dbef6f4ab24b
yingh <yinghu.lu@gmail.com>
2026-03-12 22:46:04 -0700

    rpc api

--- internal/core/event.go
+++ internal/core/event.go
@@ -1,9 +1,5 @@
 package core
 
-import (
-	"time"
-)
-
 const (
 	EVENT_FACTORY_ID uint32 = 1
 )
@@ -43,30 +39,6 @@ type Event interface {
 
 type EventCreator interface {
 	Create(classId uint32, topic string) (Event, error)
-}
-
-type Query interface {
-	QId() uint32
-	QFactoryId() uint32
-	QClassId() uint32
-	QTag() string
-	QTopic() string
-	QStartTime() time.Time
-	QEndTime() time.Time
-	QLimit() int32
-	QOffset() int32
-	QRead(b DataBuffer) error
-	QWrite(b DataBuffer) error
-	QCc() chan Chunk
-	QEvent() Event
-	QFilter(k, v []byte) bool
-}
-
-type Postoffice interface {
-	Send(e Event) error
-	List(q Query)
-	Recover(q Query)
-	Load(e Query)
 }
 
 type Publisher interface {
