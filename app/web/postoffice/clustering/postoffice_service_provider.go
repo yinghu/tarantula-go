aed5708dc8d403243efcd6bd5abedcfb4cee5edd
yingh <yinghu.lu@gmail.com>
2026-03-20 12:13:27 -0700

    event api

--- app/web/postoffice/clustering/postoffice_service_provider.go
+++ app/web/postoffice/clustering/postoffice_service_provider.go
@@ -42,12 +42,17 @@ func (c *DataServiceProvider) KeyRing(request *protocol.Request, stream grpc.Ser
 
 func (c *DataServiceProvider) Receive(request *protocol.Request, stream grpc.ServerStreamingServer[protocol.Response]) error {
 	go func() {
-		core.AppLog.Debug().Msg("startint publish")
+		core.AppLog.Debug().Msg("starting publish")
 		for {
-			stream.Send(&protocol.Response{Successful: true, Message: "hello"})
+			core.AppLog.Debug().Msg("sending message")
+			err := stream.Send(&protocol.Response{Successful: true, Message: "hello"})
+			if err != nil {
+				core.AppLog.Debug().Msgf("send error %s", err.Error())
+			}
 			time.Sleep(5 * time.Second)
 		}
 	}()
+	core.AppLog.Debug().Msg("stop publish")
 	return nil
 }
 
