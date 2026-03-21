cc4fc836ed0183b9005fbd40ed7185e0265d3c1b
yingh <yinghu.lu@gmail.com>
2026-03-21 00:13:25 -0700

    event api

--- internal/event/event_test.go
+++ internal/event/event_test.go
@@ -32,7 +32,7 @@ func (s *SampleCreator) OnEvent(e core.Event) {
 	fmt.Printf("On event %v\n", e)
 
 }
-func (s *SampleCreator) Send(e core.Event) error {
+func (s *SampleCreator) Publish(e core.Event) error {
 	return nil
 }
 func (s *SampleCreator) List(q core.Query)    {}
