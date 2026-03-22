61ff0f45b955720a94b0ce5a6346ecaa1bbdf045
yingh <yinghu.lu@gmail.com>
2026-03-22 14:41:06 -0700

    event api

--- app/web/postoffice/postoffice_service.go
+++ app/web/postoffice/postoffice_service.go
@@ -2,38 +2,15 @@ package main
 
 import (
 	"fmt"
-	"sync"
-	"time"
 
 	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/core"
-	"gameclustering.com/internal/event"
 	"gameclustering.com/postoffice/clustering"
 )
 
-type TopicMap struct {
-	sync.RWMutex
-	topics map[int32]event.SubscriptionEvent
-}
-
-type CChange struct {
-	nodeName string
-	endpoint string
-	started  bool
-}
-
 type PostofficeService struct {
 	bootstrap.AppManager
-	Ds core.DataStore
-	TopicMap
-	outboundQ chan core.Event
-	cchangeQ  []chan CChange
-
-	inboundQ chan core.Event
-	indexQ   chan core.Event
-	topicQ   []chan event.SubscriptionEvent
-	ready    sync.WaitGroup
-	mm       *clustering.MemberlistManager
+	mm *clustering.MemberlistManager
 }
 
 func (s *PostofficeService) Config() string {
@@ -45,21 +22,7 @@ func (s *PostofficeService) Start(env core.Env, p core.Pusher) error {
 	s.AppManager.Start(env, p)
 
 	s.createSchema()
-	s.topics = make(map[int32]event.SubscriptionEvent)
-	s.loadTopics()
-	s.ready = sync.WaitGroup{}
-	s.ready.Add(1)
-	s.outboundQ = make(chan core.Event, 10)
-	s.cchangeQ = make([]chan CChange, 0)
-	ec := make(chan CChange, 1)
-	s.cchangeQ = append(s.cchangeQ, ec)
-	go s.outboundEvent(ec)
-	s.indexQ = make(chan core.Event, 10)
-	s.inboundQ = make(chan core.Event, 10)
-	s.topicQ = make([]chan event.SubscriptionEvent, 0)
-	tc := make(chan event.SubscriptionEvent, 1)
-	s.topicQ = append(s.topicQ, tc)
-	go s.inboundEvent(tc)
+
 	m := clustering.MemberlistManager{StoreDir: fmt.Sprintf("%s/%s", env.HomeDir, env.GroupName)}
 	m.Seed = []string{"192.168.1.11", "192.168.1.6"}
 	m.Binding = env.NodeName
@@ -69,7 +32,7 @@ func (s *PostofficeService) Start(env core.Env, p core.Pusher) error {
 		return err
 	}
 	s.mm = &m
-	s.ready.Done()
+
 	core.AppLog.Printf("postoffice service started %s %s", env.HttpBinding, env.HomeDir)
 	return nil
 }
@@ -78,144 +41,4 @@ func (s *PostofficeService) Shutdown() {
 	core.AppLog.Println("postoffice service shutting down ...")
 	s.AppManager.Shutdown()
 	s.mm.ShutdownHook()
-}
-
-func (s *PostofficeService) Create(classId uint32, topic string) (core.Event, error) {
-	me := event.CreateEvent(classId)
-	me.OnListener(s)
-	me.OnTopic(topic)
-	if me == nil {
-		return nil, fmt.Errorf("event ( %d ) not supported", classId)
-	}
-	return me, nil
-}
-
-func (s *PostofficeService) OnError(e core.Event, err error) {
-	core.AppLog.Printf("On event error %s\n", err.Error())
-}
-
-func (s *PostofficeService) Index(e core.Event) {
-	core.AppLog.Printf("On event index %v\n", e)
-	s.indexQ <- e
-}
-
-func (s *PostofficeService) OnEvent(e core.Event) {
-	se, isSe := e.(*event.SubscriptionEvent)
-	if isSe {
-		for i := range s.topicQ {
-			s.topicQ[i] <- *se
-		}
-		return
-	}
-	s.inboundQ <- e
-}
-func (s *PostofficeService) LocalStore() core.DataStore {
-	return s.Ds
-}
-
-
-func (s *PostofficeService) NodeStarted(n core.Node) {
-	core.AppLog.Printf("node started : %s\n", n.TcpEndpoint)
-	for i := range s.cchangeQ {
-		s.cchangeQ[i] <- CChange{nodeName: n.Name, endpoint: n.TcpEndpoint, started: true}
-	}
-}
-
-func (s *PostofficeService) NodeStopped(n core.Node) {
-	core.AppLog.Printf("node stopped : %s\n", n.TcpEndpoint)
-	for i := range s.cchangeQ {
-		s.cchangeQ[i] <- CChange{nodeName: n.Name, started: false}
-	}
-}
-
-func (s *PostofficeService) onRetry(e core.Event) {
-	core.AppLog.Printf("Retrying %v\n", e)
-}
-func (s *PostofficeService) inboundEvent(t chan event.SubscriptionEvent) {
-	topics := make([]event.SubscriptionEvent, 0)
-	s.ready.Wait()
-	core.AppLog.Printf("inbound queue is ready with pending event size %d\n", len(s.inboundQ))
-	s.RLock()
-	for _, t := range s.topics {
-		topics = append(topics, t)
-	}
-	s.RUnlock()
-	for {
-		select {
-		case c := <-t:
-			topics = append(topics, c)
-		case e := <-s.inboundQ:
-			core.AppLog.Printf("Inbound event %v\n", e)
-			//e.OnIndex(s)
-		case e := <-s.indexQ:
-			for i := range topics {
-				topic := topics[i]
-				if topic.Name != e.Topic() {
-					continue
-				}
-				url := fmt.Sprintf("%s%s%s%s%s%d", "http://", topic.App, ":8080/", topic.App, "/clusteradmin/event/", e.ClassId())
-				core.AppLog.Printf("Pushlish to %s\n", url)
-				s.PostJsonSync(url, e)
-			}
-		}
-	}
-}
-func (s *PostofficeService) outboundEvent(c chan CChange) {
-	pubs := make(map[string]core.Publisher)
-	localListener := LocalEventListener{s}
-	for {
-		select {
-		case e := <-s.outboundQ:
-			ticket, err := s.Authenticator().CreateTicket(0, 0, core.ADMIN_ACCESS_CONTROL, bootstrap.TICKET_TIME_OUT_MINUTES)
-			if err != nil {
-				core.AppLog.Printf("Ticket error %s\n", err.Error())
-				continue
-			}
-			retrying := true
-			for _, pub := range pubs {
-				e.OnListener(&localListener)
-				for i := range 3 {
-					if err := pub.Publish(e, ticket); err != nil {
-						//break
-						core.AppLog.Printf("reconnect to %s retries: %d", err.Error(), i)
-						time.Sleep(500 * time.Millisecond)
-						pub.Connect()
-						continue
-					}
-					break
-				}
-				retrying = false
-			}
-			if retrying {
-				s.onRetry(e)
-			}
-		case c := <-c:
-			core.AppLog.Printf("Node Updated : %v\n", c)
-			if c.started {
-				//if c.nodeName == s.Cluster().Local().Name {
-				//pubs[c.nodeName] = &LocalPublisher{s}
-				//} else {
-				sb := event.TcpPublisher{Remote: c.endpoint}
-				for i := range 5 {
-					err := sb.Connect()
-					if err != nil {
-						core.AppLog.Printf("cannot to dial to %s retries: %d", err.Error(), i)
-						time.Sleep(1 * time.Second)
-						continue
-					}
-					core.AppLog.Printf("connected %s\n", c.endpoint)
-					pubs[c.nodeName] = &sb
-					break
-				}
-				//}
-			} else {
-				pub, rd := pubs[c.nodeName]
-				if !rd {
-					continue
-				}
-				pub.Close()
-				delete(pubs, c.nodeName)
-			}
-		}
-	}
 }
