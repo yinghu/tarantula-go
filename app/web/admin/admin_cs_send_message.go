c6097ce0d3e1dca4e587a8260bf063a16d07332d
yingh <yinghu.lu@gmail.com>
2026-03-25 13:32:31 -0700

    event api

--- app/web/admin/admin_cs_send_message.go
+++ app/web/admin/admin_cs_send_message.go
@@ -33,7 +33,7 @@ func (s *CSMessager) Request(rs core.OnSession, w http.ResponseWriter, r *http.R
 	me.OnOId(id)
 	me.Source = s.Context()
 	me.DateTime = time.Now()
-	me.OnTopic("message")
+	//me.OnTopic("message")
 	err = s.Publish(&me)
 	if err != nil {
 		w.Write(util.ToJson(core.OnSession{Successful: false, Message: err.Error()}))
