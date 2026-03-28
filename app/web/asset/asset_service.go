e06ec7191f338ff2f7c486cbf4897199d001b1bc
yingh <yinghu.lu@gmail.com>
2026-03-28 12:27:58 -0700

    event api

--- app/web/asset/asset_service.go
+++ app/web/asset/asset_service.go
@@ -25,7 +25,7 @@ func (s *AssetService) Start(f core.Env, p core.Pusher) error {
 	if err != nil {
 		return nil
 	}
-	s.Subscribe("ban", s)
+	s.Cluster().Subscribe("ban", s)
 	core.AppLog.Printf("Asset service started %s %s\n", f.HttpBinding, s.assetDir)
 	http.Handle("/asset/upload/{name}", bootstrap.Logging(&AssetUpload{AssetService: s}))
 	http.Handle("/asset/download/{name}", bootstrap.Logging(&AssetDownload{AssetService: s}))
