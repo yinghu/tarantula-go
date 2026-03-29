d91d8c6319a7a4cee4b87b738697613d275624b0
yingh <yinghu.lu@gmail.com>
2026-03-29 00:09:34 -0700

    event api

--- internal/core/app_logger.go
+++ internal/core/app_logger.go
@@ -11,13 +11,14 @@ var (
 )
 
 func CreateAppLog(dir string, truncated bool, standAlone bool) {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 	if standAlone {
 		CreateTestLog()
 		return
 	}
 	err := os.MkdirAll(dir+"/log", 0755)
 	if err != nil {
-		AppLog = zerolog.New(os.Stderr)
+		CreateTestLog()
 		return
 	}
 	opt := os.O_WRONLY | os.O_CREATE | os.O_APPEND
@@ -26,14 +27,14 @@ func CreateAppLog(dir string, truncated bool, standAlone bool) {
 	}
 	file, err := os.OpenFile(dir+"/log/tarantula.log", opt, 0644)
 	if err != nil {
-		AppLog = zerolog.New(os.Stderr)
+		CreateTestLog()
 		return
 	}
-	AppLog = zerolog.New(file)
+	AppLog = zerolog.New(file).With().Timestamp().Logger().With().Caller().Logger()
 	AppLog.Info().Msg("Initialized app log")
 }
 
 func CreateTestLog() {
-	AppLog = zerolog.New(os.Stderr)
+	AppLog = zerolog.New(os.Stderr).With().Timestamp().Logger().With().Caller().Logger()
 	AppLog.Info().Msg("Initialized test app log")
 }
