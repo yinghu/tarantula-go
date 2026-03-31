c32c602a5a6e64d045fd809969292487797d4c36
yinghu <yinghu.lu@gmail.com>
2026-03-31 10:11:11 -0700

    tcp limit

--- app/web/postoffice/clustering/data_service_proxy.go
+++ app/web/postoffice/clustering/data_service_proxy.go
@@ -11,55 +11,6 @@ import (
 	"google.golang.org/grpc"
 )
 
-func (c *DataServiceProvider) runCreate(set *protocol.Request) (*protocol.Response, error) {
-	rq := make(chan []core.Node, 3)
-	defer close(rq)
-	retry := RetryTrack{Reties: RETRY_MAX}
-	var rt uint32
-	if set.Prefix > 0 {
-		rt = set.Prefix
-		core.AppLog.Debug().Msgf("using prefix %d", set.Prefix)
-	} else {
-		rt = c.Mll.RingToken(set.Data.Key)
-	}
-	ch := make(chan *protocol.Response, 3)
-	defer close(ch)
-	for retry.Reties > 0 {
-		c.Mll.MRequest <- core.RingRequest{Opt: REPLICA_RING_OPT, Token: rt, Replicas: REPLICA_MAX, Async: rq}
-		nodes := <-rq
-		ringNode := nodes[0]
-		c.clientCreate(&ringNode, set, ch)
-		resp := <-ch
-		if !resp.Successful {
-			retry.Err = resp.Message
-			retry.Reties--
-			continue
-		}
-		retry.Suc = true
-		slaves := nodes[1:]
-		for _, slave := range slaves {
-			c.clientCreate(&slave, set, ch)
-			resp = <-ch
-			if !resp.Successful {
-				core.AppLog.Debug().Msgf("error on slave %s", resp.Message)
-			}
-		}
-		break
-	}
-	core.AppLog.Printf("retry %s, %d %v", retry.Err, retry.Reties, retry.Suc)
-	return &protocol.Response{Successful: retry.Suc, Message: retry.Err}, nil
-}
-
-func (m *DataServiceProvider) clientCreate(target *core.Node, request *protocol.Request, ch chan *protocol.Response) {
-	task := Task{target: target.RpcEndpoint, execute: func(tcp *grpc.ClientConn, opt int) error {
-		dsp := protocol.NewDataServiceClient(tcp)
-		resp, _ := dsp.Create(context.Background(), request)
-		ch <- resp
-		return nil
-	}}
-	m.WTask <- task
-}
-
 func (c *DataServiceProvider) runUpdate(set *protocol.Request) (*protocol.Response, error) {
 	rq := make(chan []core.Node, 3)
 	defer close(rq)
---
+++ app/web/postoffice/clustering/run_create_proxy.go
@@ -0,0 +1,67 @@
+package clustering
+
+import (
+	context "context"
+	"fmt"
+
+	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/protocol"
+	"google.golang.org/grpc"
+)
+
+func (c *DataServiceProvider) runCreate(set *protocol.Request) (*protocol.Response, error) {
+	rq := make(chan []core.Node, 3)
+	defer close(rq)
+	retry := RetryTrack{Reties: RETRY_MAX}
+	var rt uint32
+	if set.Prefix > 0 {
+		rt = set.Prefix
+		core.AppLog.Debug().Msgf("using prefix %d", set.Prefix)
+	} else {
+		rt = c.Mll.RingToken(set.Data.Key)
+	}
+	ch := make(chan *protocol.Response, 3)
+	defer close(ch)
+	for retry.Reties > 0 {
+		c.Mll.MRequest <- core.RingRequest{Opt: REPLICA_RING_OPT, Token: rt, Replicas: REPLICA_MAX, Async: rq}
+		nodes := <-rq
+		ringNode := nodes[0]
+		c.clientCreate(&ringNode, set, ch)
+		resp := <-ch
+		if !resp.Successful {
+			retry.Err = resp.Message
+			retry.Reties--
+			continue
+		}
+		retry.Suc = true
+		slaves := nodes[1:]
+		for _, slave := range slaves {
+			c.clientCreate(&slave, set, ch)
+			resp = <-ch
+			if !resp.Successful {
+				core.AppLog.Debug().Msgf("error on slave %s", resp.Message)
+			}
+		}
+		break
+	}
+	core.AppLog.Printf("retry %s, %d %v", retry.Err, retry.Reties, retry.Suc)
+	return &protocol.Response{Successful: retry.Suc, Message: retry.Err}, nil
+}
+
+func (m *DataServiceProvider) clientCreate(target *core.Node, request *protocol.Request, ch chan *protocol.Response) {
+	task := Task{target: target.RpcEndpoint, execute: func(tcp *grpc.ClientConn, opt int) error {
+		if opt == NO_TCP_CONNECT {
+			ch <- &protocol.Response{Successful: false, Message: "no tcp connect"}
+			return fmt.Errorf("no tcp connect")
+		}
+		dsp := protocol.NewDataServiceClient(tcp)
+		resp, err := dsp.Create(context.Background(), request)
+		if err != nil {
+			ch <- &protocol.Response{Successful: false, Message: err.Error()}
+			return err
+		}
+		ch <- resp
+		return nil
+	}}
+	m.WTask <- task
+}
