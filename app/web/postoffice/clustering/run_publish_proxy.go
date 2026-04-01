143a236e0073579ca7fca2303ce94aed33691880
yinghu <yinghu.lu@gmail.com>
2026-04-01 14:04:56 -0700

    proto hook

--- app/web/postoffice/clustering/run_publish_proxy.go
+++ app/web/postoffice/clustering/run_publish_proxy.go
@@ -14,7 +14,7 @@ func (c *DataServiceProvider) runPublish(topic *protocol.Topic) (*protocol.Respo
 	rc := make(chan *protocol.Response, 1)
 	defer close(rc)
 
-	tpf, registered := bootstrap.TopicFactoryRegistry[fmt.Sprintf("%sEventFactory", topic.Name)]
+	tpf, registered := bootstrap.TopicFactoryRegistry["MessageEventFactory"]
 	if !registered {
 		return &protocol.Response{Successful: false}, fmt.Errorf("event factory not registered")
 	}
