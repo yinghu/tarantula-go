079d817512341e937c885c4399ef78b83bc92af8
yinghu <yinghu.lu@gmail.com>
2026-04-05 02:08:13 -0700

    tcp event re-api

--- app/web/postoffice/clustering/run_create_proxy.go
+++ app/web/postoffice/clustering/run_create_proxy.go
@@ -45,7 +45,7 @@ func (c *DataServiceProvider) runCreate(set *protocol.Request) (*protocol.Respon
 		}
 		break
 	}
-	core.AppLog.Printf("retry %s, %d %v", retry.Err, retry.Reties, retry.Suc)
+	core.AppLog.Debug().Msgf("retry %s, %d %v", retry.Err, retry.Reties, retry.Suc)
 	return &protocol.Response{Successful: retry.Suc, Message: retry.Err}, nil
 }
 
