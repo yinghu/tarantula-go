544d634bd8e623e091973961563fe33b27cb98bc
yinghu <yinghu.lu@gmail.com>
2026-04-08 13:46:29 -0700

    rpc pool hook

--- app/web/postoffice/clustering/run_publish_proxy.go
+++ app/web/postoffice/clustering/run_publish_proxy.go
@@ -7,21 +7,16 @@ import (
 	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/protocol"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 )
 
 func (c *DataServiceProvider) runPublish(topic *protocol.Topic) (*protocol.Response, error) {
-	rc := make(chan *protocol.Response, 1)
-	defer close(rc)
-
 	tpf, registered := bootstrap.TopicFactoryRegistry[topic.Name]
 	if !registered {
 		return &protocol.Response{Successful: false}, fmt.Errorf("event factory not registered")
 	}
 	req, err := tpf().Request(topic)
 	if err != nil {
-		rc <- &protocol.Response{Successful: false}
+		return &protocol.Response{Successful: false}, err
 	}
 	resp, err := c.runCreate(req)
 	if !resp.Successful {
@@ -33,20 +28,15 @@ func (c *DataServiceProvider) runPublish(topic *protocol.Topic) (*protocol.Respo
 	c.DRequest <- TopicRequest{Opt: TOPIC_REGISTER, Subs: rq, NodeId: topic.NodeId, Tag: topic.Tag, Name: topic.Name}
 	subs := <-rq
 	for _, sub := range subs {
-		c.clientPublish(&sub, topic, rc)
-		resp = <-rc
+		c.clientPublish(&sub, topic)
 	}
 	return &protocol.Response{Successful: true, Message: "topic delivered"}, nil
 }
-func (m *DataServiceProvider) clientPublish(target *core.Subscription, request *protocol.Topic, async chan *protocol.Response) {
-	core.AppLog.Debug().Msgf("SUB POOL %s", target.CPool.Target)
-	tcp, err := grpc.NewClient(target.Endpoint, grpc.WithTransportCredentials(insecure.NewCredentials()))
+func (m *DataServiceProvider) clientPublish(target *core.Subscription, request *protocol.Topic) (*protocol.Response, error) {
+	conn, err := target.CPool.Conn()
 	if err != nil {
-		async <- &protocol.Response{Successful: false, Message: err.Error()}
-		return
+		return &protocol.Response{Successful: false, Message: err.Error()}, err
 	}
-	defer tcp.Close()
-	dsp := protocol.NewDataServiceClient(tcp)
-	resp, err := dsp.Send(context.Background(), request)
-	async <- resp
+	dsp := protocol.NewDataServiceClient(conn.Conn)
+	return dsp.Send(context.Background(), request)
 }
