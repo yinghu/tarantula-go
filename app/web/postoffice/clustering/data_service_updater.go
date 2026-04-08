d992aa374512a0b2f09372fa45241ffb9f40758d
yinghu <yinghu.lu@gmail.com>
2026-04-08 11:47:39 -0700

    rpc pool hook

--- app/web/postoffice/clustering/data_service_updater.go
+++ app/web/postoffice/clustering/data_service_updater.go
@@ -86,6 +86,8 @@ func (m *DataServiceProvider) registerSubscription(sub core.Subscription) {
 	if sub.Deleting {
 		m.subscriptions.del(sub)
 	} else {
+		sub.CPool = &core.RpcConnPool{Target: sub.Endpoint, Tag: sub.Tag, NodeId: sub.NodeId}
+		sub.CPool.Start()
 		m.subscriptions.add(sub)
 	}
 }
