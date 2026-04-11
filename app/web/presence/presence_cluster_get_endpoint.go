7ba23d46d8dedbe6d0ba677b2813b7b27a112fdf
yinghu <yinghu.lu@gmail.com>
2026-04-11 09:41:38 -0700

    proto api

--- app/web/presence/presence_cluster_get_endpoint.go
+++ app/web/presence/presence_cluster_get_endpoint.go
@@ -3,8 +3,8 @@ package main
 import (
 	"net/http"
 
-	"gameclustering.com/internal/bootstrap"
 	"gameclustering.com/internal/core"
+	"gameclustering.com/internal/persistence"
 	"gameclustering.com/internal/protocol"
 	"gameclustering.com/internal/util"
 )
@@ -20,19 +20,20 @@ func (s *PresenceClusterGet) AccessControl() int32 {
 func (s *PresenceClusterGet) Request(rs core.OnSession, w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	kn := r.PathValue("key")
-
-	var co bootstrap.ClusterObject
-	co.Key = kn
-	k, _, err := core.Export(&co, 200)
+	login := protocol.LoginObject{Name: kn}
+	mf := persistence.NewLoginObjectFactory()
+	kv, err := mf.FromLoginObject(&login)
 	if err != nil {
 		w.Write(util.ToJson(core.OnSession{Successful: false, Message: err.Error()}))
 		return
 	}
-	//req := core.DataRequest{Key: k, Opt: core.GET_DATA_REQUEST}
-	//req.FactoryId = co.FactoryId()
-	//req.ClassId = co.ClassId()
-	q := protocol.Request{Opt: core.GET_DATA_REQUEST, Data: &protocol.Data{Key: k, Header: &protocol.Header{FactoryId: co.FactoryId(), ClassId: co.ClassId()}}}
-	resp, err := s.Cluster().Request(&q)
+	req, err := mf.Request(kv)
+	if err != nil {
+		w.Write(util.ToJson(core.OnSession{Successful: false, Message: err.Error()}))
+		return
+	}
+	req.Opt = core.GET_DATA_REQUEST
+	resp, err := s.Cluster().Request(req)
 	if err != nil {
 		w.Write(util.ToJson(core.OnSession{Successful: false, Message: err.Error()}))
 		return
