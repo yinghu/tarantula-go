32c55fb582588df20faed1996d110662ac41bfce
yingh <yinghu.lu@gmail.com>
2026-04-13 22:17:03 -0700

    rpc proto

--- app/web/postoffice/clustering/task_service_provider.go
+++ app/web/postoffice/clustering/task_service_provider.go
@@ -8,11 +8,6 @@ import (
 )
 
 func (c *DataServiceProvider) Run(ctx context.Context, in *protocol.Transaction) (*protocol.Response, error) {
-	//msg := make(chan *protocol.Response, 1)
-	//defer close(msg)
-	//setData := SetData{Opt: in.Opt, Data: in.Data, Resp: msg}
-	//c.DSet <- setData
-	//resp := <-msg
 	core.AppLog.Debug().Msg("running task on target node")
 	c.DMessager <- &protocol.Mail{Transaction: in, Opt: core.TRANS_MAIL}
 	return &protocol.Response{Successful: true, Message: "run task"}, nil
