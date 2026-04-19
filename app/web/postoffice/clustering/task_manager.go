ef8f78abe0593d2d3a9b98b3822ee083433294dc
yingh <yinghu.lu@gmail.com>
2026-04-18 22:38:21 -0700

    txn api

--- app/web/postoffice/clustering/task_manager.go
+++ app/web/postoffice/clustering/task_manager.go
@@ -108,6 +108,7 @@ func (m *TaskManager) Wait() {
 				if tr.confirmed == 0 {
 					m.confirmed(tr)
 				}
+				core.AppLog.Debug().Msgf("task confirmed %v", meta)
 				//m.s.DMessager <- &protocol.Mail{Transaction: &protocol.Transaction{Meta: meta}, Opt: core.TRANS_MAIL}
 			case protocol.TCC_CANCELED:
 				m.canceled(tr)
