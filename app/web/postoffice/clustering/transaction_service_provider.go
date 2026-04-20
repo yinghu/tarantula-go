43404164291c2e78e7f7c866bd31740829740804
yingh <yinghu.lu@gmail.com>
2026-04-19 23:46:45 -0700

    rpc proto

--- app/web/postoffice/clustering/transaction_service_provider.go
+++ app/web/postoffice/clustering/transaction_service_provider.go
@@ -78,6 +78,7 @@ func (c *DataServiceProvider) updateTask(t *protocol.Task) {
 		return
 	}
 	req.Data.Header.Revision = 1
+	req.Opt = core.UPDATE_DATA_REQUEST
 	resp, err := c.runUpdate(req)
 	if err != nil {
 		core.AppLog.Warn().Msgf("cannot update %s", err.Error())
