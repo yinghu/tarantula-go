7cff45bff67e45a7ae6d326616fe35b892b3e566
yingh <yinghu.lu@gmail.com>
2026-04-19 22:44:42 -0700

    rpc proto

--- app/web/inventory/inventory_service.go
+++ app/web/inventory/inventory_service.go
@@ -35,10 +35,11 @@ func (s *InventoryService) Start(f core.Env) error {
 	}})
 	s.Cluster().Register("grant", &protocol.TccTransationListener{Reserve: func(e *protocol.Transaction) error {
 		core.AppLog.Debug().Msgf("reserve resource %v", e)
-		time.Sleep(100 * time.Second)
+		//time.Sleep(100 * time.Second)
 		return nil //fmt.Errorf("no resource")
 	}, Confirm: func(e *protocol.Transaction) error {
 		core.AppLog.Debug().Msgf("confirm resource %v", e)
+		time.Sleep(100 * time.Second)
 		return nil
 	}, Cancel: func(e *protocol.Transaction) error {
 		core.AppLog.Debug().Msgf("cancel resource %v", e)
