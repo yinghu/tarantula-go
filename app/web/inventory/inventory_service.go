f6f108dea59a0d7a0d095e254f862e953d2daf65
yingh <yinghu.lu@gmail.com>
2026-04-23 10:58:35 -0700

    task api

--- app/web/inventory/inventory_service.go
+++ app/web/inventory/inventory_service.go
@@ -34,10 +34,15 @@ func (s *InventoryService) Start(f core.Env) error {
 	}})
 	s.Cluster().Register("grant", &protocol.TccTransationListener{Reserve: func(e *protocol.Transaction) error {
 		core.AppLog.Debug().Msgf("reserve resource %v", e)
-		//time.Sleep(100 * time.Second)
+		e.Meta.NodeId = s.NodeId()
+		e.Meta.Tag = s.Context()
+		e.Meta.Name = "grant"
 		return nil //fmt.Errorf("no resource")
 	}, Confirm: func(e *protocol.Transaction) error {
 		core.AppLog.Debug().Msgf("confirm resource %v", e)
+		e.Meta.NodeId = s.NodeId()
+		e.Meta.Tag = s.Context()
+		e.Meta.Name = "grant"
 		//time.Sleep(100 * time.Second)
 		return nil
 	}, Cancel: func(e *protocol.Transaction) error {
