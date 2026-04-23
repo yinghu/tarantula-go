1f47ca272efd9adfa7182e85ea10e501a1df0c58
yingh <yinghu.lu@gmail.com>
2026-04-23 12:02:51 -0700

    task api

--- internal/event/transaction_event_factory.go
+++ internal/event/transaction_event_factory.go
@@ -1,14 +1,11 @@
 package event
 
 import (
-	"time"
-
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/protocol"
 
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/anypb"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 func NewTransactionEventFactory() *TransactionEventFactory {
@@ -26,13 +23,13 @@ func NewTransactionEventFactory() *TransactionEventFactory {
 type TransactionEventFactory struct {
 	ProtoTopicFactoryObj
 }
-func (p *TransactionEventFactory) GetRequest(key []byte) *protocol.Request{
-	return &protocol.Request{Opt: core.GET_DATA_REQUEST,Data: &protocol.Data{Key:key,Header: &protocol.Header{FactoryId: core.EVENT_FACTORY_ID, ClassId: TRANSACTION_EVENT_CID}}}
+
+func (p *TransactionEventFactory) GetRequest(key []byte) *protocol.Request {
+	return &protocol.Request{Opt: core.GET_DATA_REQUEST, Data: &protocol.Data{Key: key, Header: &protocol.Header{FactoryId: core.EVENT_FACTORY_ID, ClassId: TRANSACTION_EVENT_CID}}}
 }
 func (p *TransactionEventFactory) FromTransactionEvent(e *protocol.TransactionEvent) (*protocol.Topic, error) {
 	tpx := protocol.Topic{Name: TRANSACTION_TOPIC_NAME}
-	msg := protocol.Event{Key:&protocol.Key{Header: &protocol.Header{FactoryId: core.EVENT_FACTORY_ID, ClassId: TRANSACTION_EVENT_CID}}}
-	e.Time = timestamppb.New(time.Now())
+	msg := protocol.Event{Key: &protocol.Key{Header: &protocol.Header{FactoryId: core.EVENT_FACTORY_ID, ClassId: TRANSACTION_EVENT_CID}}}
 	obj, err := anypb.New(e)
 	if err != nil {
 		return &tpx, err
