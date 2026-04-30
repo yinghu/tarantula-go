6e439f7cbd0b78fb9eff26f11b262baaca06cee6
yingh <yinghu.lu@gmail.com>
2026-04-30 14:12:30 -0700

    task api

--- internal/event/log_event_query.go
+++ internal/event/log_event_query.go
@@ -1,6 +1,8 @@
 package event
 
 import (
+	"time"
+
 	"gameclustering.com/internal/core"
 	"gameclustering.com/internal/protocol"
 )
@@ -16,7 +18,7 @@ func (q *LogEventQuery) QFilter(k, v []byte) bool {
 		core.AppLog.Warn().Msgf("wrong decode format %s", err)
 		return false
 	}
-	core.AppLog.Debug().Msgf("topic %v", t.Event.Key.Header)
+	core.AppLog.Debug().Msgf("topic %v", time.UnixMilli(int64(t.Event.Key.Header.Timestamp)))
 	obj, err := mf.Message(t)
 	if err != nil {
 		core.AppLog.Warn().Msgf("wrong decode format %s", err)
