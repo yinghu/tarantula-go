83c90c36387b34bde8d92aebf94745b94af114e8
yingh <yinghu.lu@gmail.com>
2026-05-01 07:28:00 -0700

    task api

--- app/web/postoffice/clustering/transaction_service_provider.go
+++ app/web/postoffice/clustering/transaction_service_provider.go
@@ -87,12 +87,12 @@ func (c *DataServiceProvider) updateTask(t *TaskResource, clear ClearResource) {
 	}
 	t.revision++
 	core.AppLog.Info().Msgf("saved %v", resp)
-	tx, err := c.load(t.resource.Meta.Id)
-	if err != nil {
-		core.AppLog.Error().Msgf("no task loaded %s %d", err.Error(), t.resource.Meta.Id)
-		return
-	}
-	core.AppLog.Debug().Msgf("v %v", tx.resource.Validator.Meta)
-	core.AppLog.Debug().Msgf("j %v", tx.resource.Job.Meta)
-	core.AppLog.Debug().Msgf("t %v", tx.resource.Meta)
+	//tx, err := c.load(t.resource.Meta.Id)
+	//if err != nil {
+	//core.AppLog.Error().Msgf("no task loaded %s %d", err.Error(), t.resource.Meta.Id)
+	//return
+	//}
+	//core.AppLog.Debug().Msgf("v %v", tx.resource.Validator.Meta)
+	//core.AppLog.Debug().Msgf("j %v", tx.resource.Job.Meta)
+	//core.AppLog.Debug().Msgf("t %v", tx.resource.Meta)
 }
