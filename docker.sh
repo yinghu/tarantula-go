f0f7c83cb28393d5b222f8a0f96e334c7e7999a4
yingh <yinghu.lu@gmail.com>
2026-05-11 13:25:52 -0700

    init ai code

---
+++ docker.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+Clean(){
+    echo "deleting build files"
+    rm id_ed25519
+    rm known_hosts
+    rm .gitconfig
+    rm token.txt
+}
+Check(){
+    if [[ $? -ne 0 ]]; then
+        echo "build failed, try again"
+        Clean
+        exit $?
+    fi
+}
+
+if [[ -n "$1" ]]; then
+    version="$1"
+else
+    version=latest
+fi
+
+echo "Build params : ${version}"
+cp ~/.ssh/id_ed25519 .
+cp ~/.ssh/known_hosts .
+cp ~/.gitconfig .
+cp ~/token.txt .
+apps=("admin" "presence" "inventory" "asset" "postoffice")
+for app in "${apps[@]}"; do
+  echo "Current build target : $app"
+  sudo docker build --no-cache -f ./docker_application_build --build-arg app=$app --tag tarantula.$app:$version .
+  Check
+  ((seq++))
+done
+
+sudo docker build --no-cache -f ./docker_prometheus_node_exporter_build --tag tarantula.node:$version .
+Check
+
+sudo docker build --no-cache -f ./docker_prometheus_build --tag tarantula.prometheus:$version .
+Check
+
+sudo docker build --no-cache  -f ./docker_nginx_build --tag tarantula.nginx:$version .
+Check
+sudo docker image prune -af
+
+Clean
