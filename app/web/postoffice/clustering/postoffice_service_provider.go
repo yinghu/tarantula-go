c57abd477593b6a0e2dd70ba68e3069956794476
yingh <yinghu.lu@gmail.com>
2026-05-14 11:27:51 -0700

    vault hook config

--- app/web/postoffice/clustering/postoffice_service_provider.go
+++ app/web/postoffice/clustering/postoffice_service_provider.go
@@ -16,11 +16,8 @@ func (c *DataServiceProvider) AuthKey(ctx context.Context, request *protocol.Req
 	core.AppLog.Info().Msgf("load auth key %s", request.Context)
 	mp := strings.Split(request.Context, "#")
 	ak := protocol.AuthKey{Context: request.Context}
-	if len(mp) != 2 {
-		core.AppLog.Warn().Msgf("wrong context error %s", request.Context)
-		return &ak, fmt.Errorf("context wrong format")
-	}
-	kv, err := c.Vault.GetSecret(mp[0], mp[1])
+	core.AppLog.Warn().Msgf("context %d %v", len(mp), mp)
+	kv, err := c.Vault.GetSecret("dev/presence", "auth")
 	if err != nil {
 		core.AppLog.Warn().Msgf("load auth key error %s", err.Error())
 		return &ak, err
