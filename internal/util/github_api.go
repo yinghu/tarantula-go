05fac8be74c8dfebedb70963dcd3d7f1eaa34e2a
yingh <yinghu.lu@gmail.com>
2026-05-18 23:05:31 -0700

    git hub api hook

--- internal/util/github_api.go
+++ internal/util/github_api.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -46,6 +47,32 @@ func (h *GitHubApi) getJson(path string, cb Callback) error {
 	req.Header.Set("X-GitHub-Api-Version", "2026-03-10")
 	req.Header.Set("User-Agent", h.Org)
 	req.Header.Set("Authorization", "Bearer "+h.Token)
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	return cb(resp)
+}
+
+func (h *GitHubApi) PostJson(path string, payload any, cb Callback) error {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+	tr := &http.Transport{
+		DisableKeepAlives:  true,
+		DisableCompression: true,
+	}
+	client := &http.Client{Transport: tr}
+	req, err := http.NewRequest("POST", fmt.Sprintf("%s/%s", "", path), bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	if h.Token != "" {
+		req.Header.Set("Authorization", "Bearer "+h.Token)
+	}
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
