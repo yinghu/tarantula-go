c55b037697936e176f0802567ca96f249fa74d09
yingh <yinghu.lu@gmail.com>
2026-05-21 13:39:17 -0700

    gprc tsl hook

--- app/web/cloud/cloud_service.go
+++ app/web/cloud/cloud_service.go
@@ -17,8 +17,8 @@ type CloudService struct {
 }
 
 func (s *CloudService) Config() string {
-	//return "./cloud-conf.json"
-	return "/etc/tarantula/cloud-conf.json"
+	return "./cloud-conf.json"
+	//return "/etc/tarantula/cloud-conf.json"
 }
 
 // https://dev.to/marrouchi/the-challenge-about-ssl-in-docker-containers-no-one-talks-about-32gh
