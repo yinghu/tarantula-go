4554807f22fbc37e562de2faae4b4a40a790fcc7
yingh <yinghu.lu@gmail.com>
2026-05-23 22:37:51 -0700

    tsl rpc hook

---
+++ app/web/postoffice/clustering/grpc_auditor.go
@@ -0,0 +1,60 @@
+package clustering
+
+import (
+	context "context"
+
+	"gameclustering.com/internal/core"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+type ServerStreamPorxy struct {
+	grpc.ServerStream
+}
+
+func (r *ServerStreamPorxy) SendMsg(m any) error {
+	return r.ServerStream.SendMsg(m)
+}
+
+func (r *ServerStreamPorxy) RecvMsg(m any) error {
+	return r.ServerStream.RecvMsg(m)
+}
+
+func (c *DataServiceProvider) auditCall(ctx context.Context, req any, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (any, error) {
+	hd, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return nil, status.Error(codes.Unauthenticated, "no ticket header")
+	}
+	ticks := hd.Get(core.RPC_TICKET_HEADER)
+	if len(ticks) == 0 {
+		return nil, status.Error(codes.Unauthenticated, "no ticket header")
+	}
+	_, err := c.auth.ValidateTicket(ticks[0])
+	if err != nil {
+		return nil, status.Error(codes.Unauthenticated, "invalid ticket")
+	}
+	core.AppLog.Debug().Msgf("valid call %s", info.FullMethod)
+	return handler(ctx, req)
+}
+
+func (c *DataServiceProvider) auditStreaming(req any, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	ctx := ss.Context()
+	hd, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Error(codes.Unauthenticated, "no ticket header")
+	}
+	ticks := hd.Get(core.RPC_TICKET_HEADER)
+	if len(ticks) == 0 {
+		return status.Error(codes.Unauthenticated, "no ticket header")
+	}
+	_, err := c.auth.ValidateTicket(ticks[0])
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "invalid ticket %s", err.Error())
+	}
+	core.AppLog.Debug().Msgf("valid streaming call %s", info.FullMethod)
+	err = handler(req, &ServerStreamPorxy{ss})
+	core.AppLog.Debug().Msgf("finished streaming call %s", info.FullMethod)
+	return err
+}
